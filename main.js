b47f8203657dfb2068ddbb91e1fb1bba3eaeb030
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 21:42:43 +0600

    Number type

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-// var true = 10 // not allow
-var mathNumber = 10;
-var accountNumberDetailsId = 104;
+var num = 123;
+var PI = 3.14;
 
+console.log(num);
+console.log(PI);
+
+var num1 = Number(45);
+var num2 = Number('45');
+var num = Number("45.6");
