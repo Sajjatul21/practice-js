53a1c309e4673c14a75a18c1bb629447644ebf55
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 13:49:29 +0600

    falsy value

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-var abc;
-var xyz = null;
-console.log(abc)
-console.log(xyz)
+var str = '1000';
+var n = 10;
+
+console.log(str * n);
+
+console.log(str + n);
+
+console.log(Number(str));
+console.log(Number(str) + n);
+console.log(parseInt(str));
+console.log(parseFloat(str));
+
+console.log(n.toString());
+
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+console.log(Boolean(-Infinity));
+
+// falsy value 
+console.log(Boolean(''))
+console.log(Boolean('jfdk'))
+console.log(Boolean(0))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(NaN))
+
+// boolean to string
+
+console.log(true)
+
+var x = true
+console.log(x.toString())
