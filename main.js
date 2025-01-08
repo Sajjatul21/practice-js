ce46f397822b31df77e35ba7ca2a218bf42df64e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 15:47:56 +0600

    ternary operator

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-var a = 10;
-var b = 40;
-var c = 20;
+var n = 10;
+var str =''
 
-if (a > b && a > c) {
-    console.log('a is greater than b and c');
-}
-else if (b > a && b > c) {
-    console.log("b is greater than a and c");
-}
-else {
-    console.log('c is large');
-}
-
-
-a = 50;
-b = 40;
-c = 20;
-
-
-if(a>b || a>c){
-    console.log('a is large')
-}
-else if (b>a|| b>c){
-    console.log('b is large')
+if (n%2== 0){
+    str ='Even'
 }
 else{
-    console.log('c is large')
+    str = "Odd"
 }
+console.log(str)
 
+// ternary Operator
 
-var check = !(a>b)
-console.log(check)
+var result = n%2 == 0?"Even":"Odd"
+console.log(result)
