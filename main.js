22e2777e753b2eb5162e48760e9f460273042aab
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 14:20:13 +0600

    Math function

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-console.log('hello world')
+console.log(Math.E)
+console.log(Math.PI)
 
-var str = 'String'
 
-var number = 10 + (30/5)*5;
-var a = 10;
+var n = -5
+console.log(Math.abs(n))
+
+n = 5.49
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(400,500,700))
+console.log(Math.min(400,500,700))
+
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(9))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.round(Math.random()*10+1))
