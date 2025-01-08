d1181d773b50f6f38d968b568943e0e082eb548b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 13:51:07 +0600

    hexadecimal and octal number

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-var str = '1000';
-var n = 10;
-
-console.log(str * n);
-
-console.log(str + n);
-
-console.log(Number(str));
-console.log(Number(str) + n);
-console.log(parseInt(str));
-console.log(parseFloat(str));
-
-console.log(n.toString());
-
-console.log(String(Infinity));
-console.log(Boolean(Infinity));
-console.log(Boolean(-Infinity));
-
-// falsy value 
-console.log(Boolean(''))
-console.log(Boolean('jfdk'))
-console.log(Boolean(0))
-console.log(Boolean(null))
-console.log(Boolean(undefined))
-console.log(Boolean(NaN))
-
-// boolean to string
-
-console.log(true)
-
-var x = true
-console.log(x.toString())
+var hex = 0xff
+var oct = 0o457
+console.log(hex)
+console.log(oct)
