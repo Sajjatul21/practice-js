1e7f20f355bc3181abc119643d52241a3f63fba6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 16:03:05 +0600

    sum

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-for (var i = 0; i<10; i++){
-    if(i%2== 0){
-        console.log(i)
-    }
+var sum = 0
+for (var i = 0; i<=10; i++){
+    console.log(sum + " + "+ i+ " = "+ (sum +i))
+    sum +=i
 }
+console.log('total sum',sum)
+
