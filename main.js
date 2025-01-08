92b622a47bca265acf2579be1c89dcf50e4c7e8f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 16:08:26 +0600

    while loop

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var sum = 0
-for (var i = 0; i<=10; i++){
-    console.log(sum + " + "+ i+ " = "+ (sum +i))
-    sum +=i
+var i = 0;
+while (i < 3) {
+    console.log(i, 'shihab');
+    i++;
 }
-console.log('total sum',sum)
 
+
+var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log('winner winner chicken dinner');
+        isRunning = false;
+    }
+    else {
+        console.log("You have got", rand);
+    }
+}
