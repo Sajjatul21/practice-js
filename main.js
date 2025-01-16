2963e432c1bdecf22e4e8054585a60d0f0f1a703
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 09:26:46 +0600

    do while loop

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-var i = 0;
-while (i < 3) {
-    console.log(i, 'shihab');
-    i++;
+var isRunning = false;
+do {
+    console.log('I am running');
 }
-
-
-var isRunning = true;
-while (isRunning) {
-    var rand = Math.floor(Math.random() * 10 + 1);
-    if (rand == 9) {
-        console.log('winner winner chicken dinner');
-        isRunning = false;
-    }
-    else {
-        console.log("You have got", rand);
-    }
-}
+while (isRunning);
