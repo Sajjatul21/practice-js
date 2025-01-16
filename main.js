16b4f34b0528c05108809ee119b0bc271be58eed
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 16:04:56 +0600

    array method

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-for (var i = 0; i < (arr.length / 2); i++) {
-    var temp = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
 
-    /*  console.log('arr elememt',arr[i])
-    console.log('temp',temp)*/
-} 
+console.log(arr.join(' '));
+console.log(arr.join(', '));
+console.log(arr.join('| '));
+console.log(arr.fill(0));
+console.log(arr.fill(true));
+
+var arr1 = [3, 4, 5, 6];
+var arr2 = [7, 8, 9, 10];
+var arr3 = arr1.concat(arr2);
+console.log(arr3)
+console.log(Array.isArray(arr))
+
+var a = [1,2]
+var b = Array.from(a)
+b[0]=100
+console.log(a)
+console.log(b)
