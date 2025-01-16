8570dabad6d8e48c12344f5bd95cb643c01ae830
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 16:10:36 +0600

    obj

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+var point = {
+    x: 10,
+    y: 20
+};
 
-console.log(arr.join(' '));
-console.log(arr.join(', '));
-console.log(arr.join('| '));
-console.log(arr.fill(0));
-console.log(arr.fill(true));
+console.log(point);
+point.x = 5;
+point.z = 30;
+console.log(point);
 
-var arr1 = [3, 4, 5, 6];
-var arr2 = [7, 8, 9, 10];
-var arr3 = arr1.concat(arr2);
-console.log(arr3)
-console.log(Array.isArray(arr))
+var obj = Object();
+obj.a = 10;
+console.log(obj);
 
-var a = [1,2]
-var b = Array.from(a)
-b[0]=100
-console.log(a)
-console.log(b)
+var obj1 = new Object();
+obj1.b = 20;
+console.log(obj1);
