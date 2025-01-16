2cf8a56628d23ab307962b8612e9cb648d24ee7f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 21:09:55 +0600

    higher order function

--- main.js
+++ main.js
@@ -2,23 +2,19 @@ function add(a, b) {
     return a + b;
 }
 
-var sum = add;
-console.log(sum(5, 5));
-console.log(typeof sum);
+function manipulate(a, b, func) {
+    var c = a + b;
+    var d = a - b;
 
-var arr = [];
-arr.push(add);
-console.log(arr);
-console.log(arr[0](5,5));
-
-var obj = {
-    sum : add
+    /*  function multiply() {
+         var m = func(a, b);
+         return c * d * m;
+     }
+     return multiply; */
+    return function () {
+        var m = func(c, d);
+        return c * d * m;
+    };
 }
-
-console.log(obj)
-console.log(obj.sum(5,5))
-
-
-setTimeout(function(){
-    console.log('I have created...')
-},100)
+var multiply = manipulate(4, 3, add);
+console.log(multiply());
