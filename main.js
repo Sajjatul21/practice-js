a121c949d29f0450ec5aded46af23dd5689bde79
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 20:28:14 +0600

    sum of array

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var obj = {
-    x: 10,
-    y: 20,
-    z: 30
-};
+var arr1 = [23, 43, 64];
+var arr2 = [65, 83, 94];
+var arr3 = [5, 85, 90];
 
-console.log(Object.keys(obj));
-console.log(Object.values(obj));
-console.log(Object.entries(obj));
+function sumOfArray(arr){
+    var sum = 0;
+    for(var i = 0;i<arr.length;i++){
+        sum += arr[i]
+    }
+    console.log(sum)
+}
 
-var obj2 = Object.assign({},obj);
-obj2.x = 999;
-console.log(obj);
-console.log(obj2);
+sumOfArray(arr1)
+sumOfArray(arr2)
+sumOfArray(arr3)
+
