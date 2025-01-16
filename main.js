bbcd15ba6d8e4e5fed6085d4673f7ebcd9958aac
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 20:14:35 +0600

    compare object

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var obj = {
     x: 10,
     y: 20,
-    z: 30
 };
 
-delete obj.x
-console.log(obj)
+var obj2 = {
+    x: 10,
+    y: 20,
+};
+
+console.log(obj == obj2)
+console.log(obj.a == obj2.a && obj.b == obj2.b)
+
+console.log(JSON.stringify(obj)== JSON.stringify(obj2))
