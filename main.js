6d7c22f60958436abff955228a751176c2ac3267
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 11:56:00 +0600

    string length

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-var a = 'I am';
-var b = "Shihab";
+var str = 'some thing';
+console.log(str.charAt(0));
+var str2 = str.charAt(200);
+console.log(typeof str2);
 
-var c = a.concat(" " + b);
-console.log(c);
 
-var d = c.substr(5, 4);
-console.log(d);
+var length = 0;
+while (true) {
+    if (str.charAt(length) == '') {
+        break;
+    }
+    else {
+        length++;
+    }
+}
+console.log(length);
 
-console.log(c.charAt(4));
-console.log(c.startsWith('I'));
-console.log(c.endsWith('Shihab'));
-console.log(a.toUpperCase());
-console.log(c.toLowerCase());
-
-console.log('   kfal  '.trim());
-
-console.log(c.split(" "));
-console.log(c.split());
+console.log('Sajjatul islam'.length)
