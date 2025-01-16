5785d246f2d351b8e485f9b5eaf0986e693a8c98
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 11:16:53 +0600

    escape notation

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-for (; ;) {
-    var rand = Math.floor(Math.random() * 10 + 1);
-    if (rand == 9) {
-        console.log('winner winner ');
-        break;
-    }
-    else {
-        console.log(rand);
-    }
-
-}
+var str = 'this is a \'string\'';
+console.log(str);
+var str1 = 'this is a \nstring';
+console.log(str1);
+var str2 = 'this is a \tstring';
+console.log(str2);
+var str3 = 'this is a \\string';
+console.log(str3);
