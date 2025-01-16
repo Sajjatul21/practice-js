fa6968106077503865d9b0e2f5323482c0e0b471
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-16 11:09:21 +0600

    continue

--- main.js
+++ main.js
@@ -19,3 +19,12 @@ for (var i = 1; i <= 10; i++) {
         console.log(i);
     }
 }
+
+for (var i = 1; i <= 10; i++) {
+    if (i == 3 || i == 7) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
+}
