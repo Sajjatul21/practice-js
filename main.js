b4e6355e2fbb748eb42b078d131d123ff8d3e5a5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-17 23:29:58 +0600

    map()

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 var arr = [1, 2, 3, 4, 5];
-var sum = 0;
-function forEach(arr, callback) {
-    for (var i = 0; i < arr.length; i++) {
-        // console.log(arr[i]);
-        callback(arr[i], i, arr);
-    }
-}
-forEach(arr, function (value, i, arr) {
-    console.log(value, i, arr);
-    sum += value;
-});
-console.log(sum);
 
-forEach(arr, function (value, index,arr) {
-    arr[index] = value + 5;
-
+var sqrArr = arr.map(function (value, index, arr) {
+    // console.log(value, index, arr);
+    // return Math.random()*100
+    return value * value
 });
-console.log(arr);
+console.log(sqrArr)
