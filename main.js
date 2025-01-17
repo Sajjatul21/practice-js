98517edc43f584ab68f53ee2dcf89c16a73b53a0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-17 23:23:40 +0600

    forEach implementation

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 var arr = [1, 2, 3, 4, 5];
 var sum = 0;
-arr.forEach(function(value, index, arr){
-    // console.log(value,index,arr)
-    sum += value
-})
+function forEach(arr, callback) {
+    for (var i = 0; i < arr.length; i++) {
+        // console.log(arr[i]);
+        callback(arr[i], i, arr);
+    }
+}
+forEach(arr, function (value, i, arr) {
+    console.log(value, i, arr);
+    sum += value;
+});
+console.log(sum);
 
-console.log(sum)
+forEach(arr, function (value, index,arr) {
+    arr[index] = value + 5;
+
+});
+console.log(arr);
