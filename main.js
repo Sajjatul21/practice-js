dd92a757bbf11bef37b801bcee5f81ae6a8e06b2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 04:34:00 +0600

    closure

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-var a = 11;
-function A() {
-    var b = 12;
-    function B() {
-        var c = 23;
-        console.log(c);
-    }
-    function C() {
-        var d = 56;
-        console.log(d);
+function test(){
+    var msg = 'I am learning scope and closure'
+
+    return function(){
+        console.log(msg)
     }
 
-    console.log(b);
-    D(b);
-    B();
-    C();
-}
-function D(n) {
-    return n + a;
 }
 
-//A()-a,b,B(),c(),d()
-//B()-a,b,c,B(),c(),d()
-//C()-a,b,,d,B(),c(),d()
-//D()-a,n,A()
+var sayMsg = test()
+sayMsg()
