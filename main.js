724f6246479c1144266befbc123332af1cb0a97f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 02:07:38 +0600

    reduce

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
 var arr = [2, 5, 2, 7, 8, 5, 4, 9, 5, 8, 4];
-
-/* function filter (arr){
-    var newArray = []
-
-    for(var i = 0;i<arr.length;i++){
-        if(arr[i]%2==0){
-            newArray.push(arr[i])
-        }
-        
-    }
-    return newArray
-}
-
-var odd = filter(arr)
-console.log(odd) */
-
-function filter(arr, callback) {
-    var newArray = [];
-
-    for (var i = 0; i < arr.length; i++) {
-        /* if (callback(arr[i])){
-        newArray.push(arr[i])
-    } */
-        callback(arr[i]) && newArray.push(arr[i]);
-    }
-    return newArray;
-}
-
-var odd = filter(arr, function (value) {
-    return value % 2 == 0;
+var sum = arr.reduce(function (previousValue, CurrentValue) {
+    // console.log(previousValue, CurrentValue)
+    // return previousValue + CurrentValue
+    return Math.max(previousValue, CurrentValue);
 });
-console.log(odd);
+console.log(sum);
