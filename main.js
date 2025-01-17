ab98adc4eac2fae1e0697b5adc48bcfc2e563188
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 04:30:08 +0600

    scope chain

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-var x = 55;
-function test (){
-    // var x = 45;
-    // console.log(45)
-    function nested(){
-        // var y = 65;
-        console.log(x)
+var a = 11;
+function A() {
+    var b = 12;
+    function B() {
+        var c = 23;
+        console.log(c);
     }
-    // console.log(y)
-    nested()
+    function C() {
+        var d = 56;
+        console.log(d);
+    }
+
+    console.log(b);
+    D(b);
+    B();
+    C();
 }
-test()
+function D(n) {
+    return n + a;
+}
+
+//A()-a,b,B(),c(),d()
+//B()-a,b,c,B(),c(),d()
+//C()-a,b,,d,B(),c(),d()
+//D()-a,n,A()
