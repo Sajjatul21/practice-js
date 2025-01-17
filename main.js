5a66d9c00205f86e8bd99a70136ff9a664d8776e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-17 23:56:53 +0600

    filter ()

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 var arr = [2, 5, 2, 7, 8, 5, 4, 9, 5, 8, 4];
 
-var filterArr = arr.filter(function(value){
-    // return value % 2==0;
-    // return value % 2==1;
-    return value > 4
-})
+/* function filter (arr){
+    var newArray = []
 
-console.log(filterArr)
+    for(var i = 0;i<arr.length;i++){
+        if(arr[i]%2==0){
+            newArray.push(arr[i])
+        }
+        
+    }
+    return newArray
+}
+
+var odd = filter(arr)
+console.log(odd) */
+
+function filter(arr, callback) {
+    var newArray = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        /* if (callback(arr[i])){
+        newArray.push(arr[i])
+    } */
+        callback(arr[i]) && newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+var odd = filter(arr, function (value) {
+    return value % 2 == 0;
+});
+console.log(odd);
