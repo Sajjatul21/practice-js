f679d9f07db481d96b786551b8e420c383175812
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 02:29:14 +0600

    findIndex

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var arr = [2, 5, 2, 7, 8, 5, 4, 9, 5, 8, 4];
 
-var result = arr.find(function (value) {
+var result = arr.findIndex(function (value) {
     // return value ===9
     return value === -9;
 });
 
-// console.log(result);
+console.log(result);
 
-function find(arr, cb) {
+function findIndex(arr, cb) {
     for (var i = 0; i < arr.length; i++) {
         if (cb(arr[i])) {
-            return arr[i];
+            return i;
         }
     }
 }
-var findElement = find(arr, function(value) {
+var findElement = findIndex(arr, function(value) {
     return value == 9;
 });
 
