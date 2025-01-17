bfc2c5a999b1d21acb4a65fbad1feb1330c16daa
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 02:27:00 +0600

    find

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var arr = [2, 5, 2, 7, 8, 5, 4, 9, 5, 8, 4];
-var sum = arr.reduce(function (previousValue, CurrentValue) {
-    // console.log(previousValue, CurrentValue)
-    // return previousValue + CurrentValue
-    return Math.max(previousValue, CurrentValue);
-});
-console.log(sum);
 
-function reduce(arr, cb, acc) {
+var result = arr.find(function (value) {
+    // return value ===9
+    return value === -9;
+});
+
+// console.log(result);
+
+function find(arr, cb) {
     for (var i = 0; i < arr.length; i++) {
-        acc = cb(acc, arr[i]);
+        if (cb(arr[i])) {
+            return arr[i];
+        }
     }
-    return acc;
 }
-var sum1 = reduce(arr, function (pre, curr) {
-    return Math.min(pre,curr)
-}, arr[0]);
-console.log(sum1)
+var findElement = find(arr, function(value) {
+    return value == 9;
+});
+
+console.log(findElement)
