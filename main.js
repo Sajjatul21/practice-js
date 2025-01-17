27e7587dead060f6a06e34507ab3a77a7cd269de
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 04:22:18 +0600

    nested scope

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var globalVar = "I am a global variable";
-
-function exampleFunction() {
-    var functionVar = "I am a function scope variable";
-    console.log(globalVar);
-    console.log(functionVar);
+var x = 55;
+function test (){
+    // var x = 45;
+    // console.log(45)
+    function nested(){
+        // var y = 65;
+        console.log(x)
+    }
+    // console.log(y)
+    nested()
 }
-exampleFunction();
-
-console.log(globalVar);
-// console.log(functionVar)
+test()
