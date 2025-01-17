a10a8e83449723626711474df3b27e75dd75ebd7
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-18 03:14:06 +0600

    every and some

--- main.js
+++ main.js
@@ -1,46 +1,13 @@
-var persons = [
-    {
-        name: "A",
-        age: 24
-    },
-    {
-        name: "B",
-        age: 19
-    },
-    {
-        name: "C",
-        age: 25
-    },
-    {
-        name: "D",
-        age: 21
-    }
-];
+
 var arr = [5, 4, 3, 8, 1, 7, 0, 6, 5, 7, -2, -8];
 
-arr.sort(function (a, b) {
-    if (a > b) {
-        return -1;
-    }
-    else if (a < b) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-});
-console.log(arr);
+var res = arr.every(function(value){
+    return value % 2 ==0
+})
+console.log(res)
 
+var res2 = arr.some(function(value){
+    return value >=0
+})
 
-persons.sort(function (a, b) {
-    if (a.age > b.age) {
-        return 1;
-    }
-    else if (a.age < b.age) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
-});
-console.log(persons);
+console.log(res2)
