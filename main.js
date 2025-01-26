4dd1ebc5d4712ee93d1ea27fa8f636e491040552
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-26 15:22:42 +0600

    object letiral

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
-class ClassName {
-    constructor(property1, property2) {
-        this.property1 = property1;
-        this.property2 = property2;
+var rect = {
+    width: 100,
+    height: 200,
+    draw: function () {
+        console.log('I am a rectangle');
+        console.log("my width is ", this.width);
+        console.log("my height is ", this.height);
     }
-    method1() {
-        console.log(`property1: ${this.property1}`);
-    }
-    method2() {
-        console.log(`property2: ${this.property2}`);
-
-    }
-    static staticMethod() {
-        console.log('this is a static method');
-    }
-}
-const obj = new ClassName("value1", "Value2");
-obj.method1();
-obj.method2();
-
-class Rect {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    calculateArea() {
-        return this.width * this.height;
-    }
-    calculateRange() {
-        return 2 * (this.width + this.height);
-    }
-}
-
-var rect1 = new Rect(10, 20);
-var rect2 = new Rect(20, 40);
-console.log(rect1.calculateArea())
-console.log(rect2.calculateArea())
+};
+// console.log(rect.width);
+rect.draw(); 
+rect.width = 200;
+rect.height = 300;
+rect.draw(); 
