07acd8de144a5c46ed9d7769fe13951f4dc078f2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-26 20:17:18 +0600

    privete properties

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var Rectangle = function (width, height) {
     this.width = width;
     this.height = height;
+
+    var position = {
+        x: 50,
+        y: 90
+    }
     var printProperties = function () {
         console.log('my width is ' + this.width);
         console.log("my height is " + this.height);
-    };
+    }.bind(this);
     this.draw = function () {
         console.log("I am rectangle");
         printProperties();
     };
 };
 var rect = new Rectangle(20, 10);
-// rect.printProperties()
+rect.draw()
