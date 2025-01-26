70b0bd256f620ec40ae038a46cb548b0c4e32058
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-26 20:23:25 +0600

    get ,set

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 var Rectangle = function (width, height) {
     this.width = width;
     this.height = height;
-
     var position = {
         x: 50,
-        y: 90
-    }
+        y: -100
+    };
     var printProperties = function () {
         console.log('my width is ' + this.width);
         console.log("my height is " + this.height);
     }.bind(this);
+
+    /*    this.getPosition = function(){
+           return position
+       }; */
+    Object.defineProperty(this, 'position', {
+        get: function () {
+            return position;
+        },
+        set: function (value) {
+            position = value;
+        }
+    });
+
     this.draw = function () {
         console.log("I am rectangle");
         printProperties();
+        console.log("Position:X = " + " " + position.x + ", Position:Y = " + position.y);
     };
 };
 var rect = new Rectangle(20, 10);
-rect.draw()
+rect.draw();
+// console.log(rect.getPosition())
+console.log(rect.position);
+
+rect.position ={
+    x: 69,
+    y: 34
+}
+
+console.log(rect.position)
