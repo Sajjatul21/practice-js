316b8fd2c1f12012419a5aa40d269537241d43b0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-26 19:23:24 +0600

    new

--- main.js
+++ main.js
@@ -18,3 +18,16 @@ var Rectangle = function (width, height) {
 
 var rect = new Rectangle(30, 40);
 rect.draw();
+
+function myNew(constructor) {
+    var obj = {};
+    Object.setPrototypeOf(obj, constructor.prototype);
+    var argsArray = Array.prototype.slice.apply(arguments);
+    console.log(argsArray)
+    constructor.apply(obj, argsArray.slice(1));
+    return obj;
+}
+
+
+var rect4 = myNew(Rectangle, 45, 30);
+rect4.draw();
