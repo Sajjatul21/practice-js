dc36ab32a4ab3809edb4fa60708ee23758c97cdd
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 11:24:31 +0600

    this give value in execution context

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-function myFunc() {
-    console.log(this);
-}
+var rect = {
+    width: 100,
+    height: 50,
+    draw: function () {
+        console.log("I am rectangle");
+        this.printProperties();
+        console.log(this);
+    },
+    printProperties: function () {
+        console.log('MY width is ' + this.width);
+        console.log("MY height is " + this.height);
+    }
+};
+rect.draw();
 
-myFunc();
-new myFunc();
+var another = {
+    width: 45,
+    height: 30,
+    print: rect.printProperties
+};
+another.print();
