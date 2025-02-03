d6cb1eb46f96904ef950559694fae47d982fee03
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 10:59:59 +0600

    class

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-var width = 10;
-var height = 20;
-
-function calculateArea(width, height) {
-    return width * height;
-}
-
-function calculateRange(width, height) {
-    return 2 * (width, height);
-}
-
-var area = calculateArea(width, height);
-var range = calculateRange(width, height);
-
-console.log(area);
-console.log(range);
 
 var rect = {
     width: 10,
@@ -30,3 +14,40 @@ var area2 = rect.calculateRange();
 
 console.log(area1);
 console.log(area2);
+
+class ClassName {
+    constructor(property1, property2) {
+        this.property1 = property1;
+        this.property2 = property2;
+    }
+    method1() {
+        console.log(`property: ${this.property1}`);
+    }
+    method2() {
+        console.log(`property2: ${this.property2}`);
+    }
+    static staticMethod() {
+        console.log("This is a static method.");
+    }
+}
+
+const obj = new ClassName("Value1", "Value2");
+console.log(obj);
+
+class Rect {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    calculateArea() {
+        return this.width * this.height;
+    }
+    calculateRange() {
+        return 2 * (this.width + this.height);
+    }
+}
+
+var rect1 = new Rect(10, 20);
+var rect2 = new Rect(30, 40);
+console.log(rect1);
+console.log(rect2);
