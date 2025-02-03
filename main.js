4b5655dd272d1cf0ff53342927cc146464be0f81
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 20:59:21 +0600

    privet property

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ var Rectangle = function (width, height) {
     this.width = width;
     this.height = height;
 
+    // var name = 'Sajjatul';
+    var position = {
+        x: 50,
+        y: -100
+    };
     var printProperties = function () {
         console.log("My width is", this.width);
         console.log("My height is", this.height);
-    };
+    }.bind(this);
     this.draw = function () {
         console.log("I am rectangle");
         printProperties();
+        console.log("Position:x =", position.x, ",", "Position:y =", position.y);
     };
 };
 var rect = new Rectangle(20, 10);
