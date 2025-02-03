9cd7c8a5f478e3f93580c1dcc3b995a52ddeed1f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 21:57:38 +0600

    property descriptor

--- main.js
+++ main.js
@@ -1,36 +1,14 @@
-var Rectangle = function (width, height) {
-    this.width = width;
-    this.height = height;
-
-    // var name = 'Sajjatul';
-    var position = {
-        x: 50,
-        y: -100
-    };
-    var printProperties = function () {
-        console.log("My width is", this.width);
-        console.log("My height is", this.height);
-    }.bind(this);
-
-    Object.defineProperty(this, 'position', {
-        get: function () {
-            return position;
-        },
-        set: function (value) {
-            position = value;
-        }
-
-    });
-    this.draw = function () {
-        console.log("I am rectangle");
-        printProperties();
-        console.log("Position:x =", position.x, ",", "Position:y =", position.y);
-    };
+var person = {
+    name: "Shihab"
 };
-var rect = new Rectangle(20, 10);
-console.log(rect.position);
-rect.position = {
-    x: 55,
-    y: 66
-};
-console.log(rect.position);
+
+console.log(person);
+console.log(person.toString());
+
+for (var i in person) {
+    console.log(i);
+}
+
+console.log(Object.keys(person));
+var descriptor = Object.getOwnPropertyDescriptor(person, 'name');
+console.log(descriptor);
