f31476fa876f0f43f41bd168067f0091f6d6d3de
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 19:47:54 +0600

    call

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-
-var Rectangle = function (width, height) {
-    this.width = width;
-    this.height = height;
-    this.draw = function () {
-        console.log("I am rectangle");
-        this.printProperties();
-        console.log(this);
-    };
-    this.printProperties = function () {
-        console.log("MY width is" + this.width);
-        console.log("My height is " + this.height);
-    };
+function myFunc() {
+    console.log(this.a + this.b);
+}
+var obj = {
+    a: 10,
+    b: 20
 };
-
-var rect = new Rectangle(4, 5);
-console.log(rect);
-rect.draw();
+myFunc.call(obj);
