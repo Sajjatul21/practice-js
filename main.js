6ec50fcffceea3c6d68886a590e96d5af8ae3c30
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-03 19:57:40 +0600

    pass by value by pass by referance

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-function myFunc(c, d) {
-    console.log(this.a + this.b + c + d);
+var n = 10;
+function change(n) {
+    n += 20;
+    console.log(n);
 }
+change(n);
+console.log(n);
+
 var obj = {
     a: 10,
     b: 20
 };
-// var testBind = myFunc.bind(obj, 10,20)
-// testBind()
-
-var testBind = myFunc.bind(obj)
-testBind(10,20)
+console.log(obj);
+function ChangeMe(obj) {
+    obj.a = obj.a + 100;
+    obj.b = obj.b + 100;
+    console.log("Function in: ", obj);
+}
+ChangeMe(obj);
+console.log("Function out: ", obj);
