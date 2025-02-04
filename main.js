a13a95b2cb242afa915355b05a6bde73734fdbf8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 12:37:16 +0600

    another composition

--- main.js
+++ main.js
@@ -1,61 +1,36 @@
-function extend(child, parent) {
-    child.prototype = Object.create(parent.prototype);
-    child.prototype.constructor = child;
+function mixing(target, ...sources) {
+    Object.assign(target, ...sources);
 }
+var canWalk = {
+    walk: function () {
+        console.log("Walking...");
+    }
+};
+var canSwim = {
+    swim: function () {
+        console.log("Swimming...");
+    }
+};
 
-function Shape(color) {
-    this.color = color;
+var canEat = {
+    eat: function () {
+        console.log("Eating...");
+    }
+};
+
+/* var person = Object.assign({}, canWalk, canEat);
+person.name = "Sajjatul Islam" */;
+function Person(name) {
+    this.name = name;
 }
-Shape.prototype.common = function () {
-    console.log('I am common function');
-};
+mixing(Person.prototype, canWalk, canEat);
+var person = new Person("Sajjatul");
+console.log(person);
 
-
-function Square(width, color) {
-    Shape.call(this, color);
-    this.width = width;
-};
-
-extend(Square, Shape);
-Square.prototype.draw = function () {
-    console.log("Draw");
-};
-Square.prototype.common = function () {
-    console.log('I am calling from Square and I have Overriding');
-
-};
-
-function Circle(radius, color) {
-    Shape.call(this, color);
-    this.radius = radius;
+function GoldFish(name) {
+    this.name = name;
 }
-extend(Circle, Shape);
-
-
-
-Circle.prototype.common = function () {
-    // Shape.prototype.common.call(this);
-    console.log('I am calling from Circle and I have Overriding');
-
-};
-
-
-var shape = new Shape();
-var sqr = new Square(57, "green");
-var c = new Circle(5, 'red');
-console.log(shape.common());
-console.log(sqr.common());
-console.log(c.common());
-
-var shapes = [shape, sqr, c];
-for (var i of shapes) {
-    i.common();
-}
-
-console.log(c instanceof Circle);
-console.log(c instanceof Shape);
-console.log(c instanceof Square);
-console.log(sqr instanceof Square);
-console.log(sqr instanceof Shape);
-console.log(shape instanceof Square)
-
+mixing(GoldFish.prototype, canEat, canSwim);
+var fish = new GoldFish("Oranda");
+console.log(fish);
+fish.swim()
