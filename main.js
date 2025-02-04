21287b348b2a3f00c3c403bfd945ccba8b7a49a5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 15:34:49 +0600

    defalult parameter problem

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function sqr(n=1) {
-   
+function sqr(n = 1) {
+
     return n * n;
 }
 console.log(sqr());
+
+function great(name = 'Sajjatul', msg) {
+    console.log(name.length);
+    console.log(`${msg}! ${name}`);
+}
+
+great(null, "Hello");
