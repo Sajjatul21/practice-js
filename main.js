b27cab02a2fcdac1a0f211e79b2f26151ae1bacd
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 12:04:42 +0600

    remove cde duplicatio

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+function extend(child, parent) {
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+}
+
 function Shape(color) {
     this.color = color;
 }
@@ -11,29 +16,28 @@ function Square(width, color) {
     this.width = width;
 };
 
-Square.prototype = Object.create(Shape.prototype);
-Square.prototype.constructor = Square;
+extend(Square, Shape);
 Square.prototype.draw = function () {
     console.log("Draw");
 };
 
 var shape = new Shape();
 var sqr = new Square(57, "green");
-// console.log(sqr.common());
+
 console.log(shape);
 console.log(sqr);
 
-function Circle() {
-
+function Circle(radius, color) {
+    Shape.call(this, color);
+    this.radius = radius;
 }
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+extend(Circle, Shape);
 Circle.prototype.common = function () {
     console.log('I am common function');
 
 };
 
-var c = new Circle();
-// console.log(c.common());
+var c = new Circle(5, 'red');
+
 console.log(c)
 
