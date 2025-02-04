7f85a0b4590d94fba916f1f25d8106c22c600bdb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 17:28:15 +0600

    weak set

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-let map = new Map([
-    ["a", 1],
-    ["b", 2],
-    ["c", 3]
-]);
+let a = { a: 10 }, b = { b: 20 };
+/* let set = new Set([a, b]);
+a = null;
+console.log(set);
+let arr = [...set]; 
+console.log(arr);*/
 
-let objKey = { name: 'Shihab' };
-map.delete(objKey);
-map.delete("c");
-console.log(map.get('a'));
-console.log(map);
-
-console.log(map.keys());
-console.log(map.values());
-console.log(map.entries());
-
-for (let [key, value] of map) {
-    console.log(key, value);
-}
-
-map.forEach(function (value, key) {
-    console.log(value, key);
-});
+let weakSet = new WeakSet([a, b]);
+console.log(weakSet);
+a = null;
+console.log(weakSet.has(a));
+console.log(weakSet.has(b));
