196ba99edc1dc67c61b16d3fd98f0d7910f90644
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 09:39:47 +0600

    instance memver and prototype member

--- main.js
+++ main.js
@@ -1,31 +1,15 @@
-var person = {
-    name: "Shihab"
-};
-
-console.log(person);
-console.log(person.toString());
-
-for (var i in person) {
-    console.log(i);
+function Square(width) {
+    this.width = width;
 }
-
-console.log(Object.keys(person));
-var descriptor = Object.getOwnPropertyDescriptor(person, 'name');
-console.log(descriptor);
-
-var baseObj = Object.getPrototypeOf(person);
-console.log(baseObj);
-var descriptor1 = Object.getOwnPropertyDescriptor(baseObj, "toString");
-console.log(descriptor1);
-
-Object.defineProperty(person, 'name', {
-    configurable: false,
-    enumerable: false,
-    writable: false
-});
-console.log(delete person.name);
-
-console.log(Object.keys(person));
-
-person.name = "Sajjatul Islam ";
-console.log(person);
+Square.prototype = {
+    draw: function () {
+        console.log("Draw");
+    },
+    // method overwrite
+    toString: function () {
+        return 'my width is ' + this.width;
+    }
+};
+var sqr = new Square(5)
+sqr.draw()
+console.log(sqr.toString())
