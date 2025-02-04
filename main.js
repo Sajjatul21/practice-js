94c999aa1be618a146a7e1f5e3a8db7dc264fead
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 17:02:30 +0600

    iterate with for loop

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function* generator() {
-    yield 1;
-    yield 2;
-    yield 3;
+var arr = [1, 2, 3];
+function* generator(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        yield collection[i];
+    }
 }
-let iterate = generator();
-/* console.log(iterate.next());
+let iterate = generator(arr);
+ console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
-console.log(iterate.next()); */
+console.log(iterate.next()); 
+/* console.log(iterate.next().value);
 console.log(iterate.next().value);
 console.log(iterate.next().value);
-console.log(iterate.next().value);
-console.log(iterate.next().value);
+console.log(iterate.next().value); */
