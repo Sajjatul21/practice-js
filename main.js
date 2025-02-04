5ff8fa5642e9b08712ce17e8bd0d93744a273a07
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-04 17:30:11 +0600

    weakMap

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 let a = { a: 10 }, b = { b: 20 };
-/* let set = new Set([a, b]);
-a = null;
-console.log(set);
-let arr = [...set]; 
-console.log(arr);*/
+let weakMap = new WeakMap([
+    [a, 45],
+    [b, 67]
+]);
 
-let weakSet = new WeakSet([a, b]);
-console.log(weakSet);
-a = null;
-console.log(weakSet.has(a));
-console.log(weakSet.has(b));
+console.log(weakMap);
