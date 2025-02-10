aa470eb319dc0db9ada905f2014524313b29f310
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 22:54:38 +0600

    children sipling

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const parent = list.parentElement;
 const children = list.children;
 console.log(parent);
 console.log(children);
+console.log(list.previousElementSibling);
+console.log(list.nextElementSibling);
+
+let li = document.querySelector("li");
+console.log(li.previousElementSibling);
+console.log(li.nextElementSibling);
+
+console.log(list.firstChild);
+console.log(list.firstElementChild);
+console.log(list.lastElementChild);
+console.log(list.lastElementChild);
