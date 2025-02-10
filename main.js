d004c8fc91e16ad2e576a25aca8941945130c5e1
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 22:47:14 +0600

    get parent and children

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
+let list = document.getElementById('list');
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.another = function () {
-
-        };
-    }
-    name = "Shihab";
-    test = function () {
-
-    };
-    draw() {
-        console.log(this);
-        console.log("Drawing");
-    }
-}
-
-let rect = new Rectangle(23, 43);
-rect.draw();
-console.log(typeof Rectangle);
+const parent = list.parentElement;
+const children = list.children;
+console.log(parent);
+console.log(children);
