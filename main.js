0a6ed76ff985cf4d04ff32a8f3ae377c65c2eeff
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 23:15:07 +0600

    create element

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ let listItem = document.getElementsByTagName('li');
     let text = i.innerHTML;
     i.innerHTML = `(${index + 1} ${text})`;
 });
+
+let li = document.createElement('li');
+li.className = 'list-group-item';
+li.setAttribute('title', 'i am four item');
+li.innerHTML = 'four';
+let list = document.getElementById('list');
+list.appendChild(li);
