1139ad115896e1293f24277560b3fb6e29a57add
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 23:04:00 +0600

    use sprade operator

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let listItem = document.getElementsByTagName('li');
-let listItems = Array.from(listItem);
-listItems.forEach((i, index) => {
+// let listItems = Array.from(listItem);
+
+[...listItem].forEach((i, index) => {
     let text = i.innerHTML;
     i.innerHTML = `(${index + 1} ${text})`;
 });
