17b1a51bf1d7fa77b7babf65ae2300725fd46b76
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-13 22:33:25 +0600

    get value from input button

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-
-
-function createElement(tagName, className, innerHTML) {
-    let tag = document.createElement(tagName);
-    tag.className = className || '';
-    tag.innerHTML = innerHTML || '';
-
-    return tag;
-}
-function append(parent, children) {
-    children.forEach(child => parent.appendChild(child));
-}
-
-let li = createElement('li', 'list-group-item', 'Four');
-let list = document.getElementById('list');
-list.appendChild(li);
-
-let p1 = createElement('p', 'lead', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sint blanditiis beatae dicta dolorum odio quas laboriosam molestias fuga omnis.');
-let p2 = createElement('p', 'lead', 'Ipsum dolor sit amet consectetur adipisicing elit. Et sint blanditiis beatae dicta dolorum odio quas laboriosam molestias fuga omnis.');
-
-let div = createElement('div');
-append(div, [p1, p2]);
-
-let container = document.getElementById('cont');
-container.appendChild(div);
+let name = document.getElementById('name');
+// console.log(name);
+name.addEventListener('keypress', function (event) {
+    // console.log(event);
+    if (event.key == "Enter") {
+        // console.log(event.target.value);
+        document.getElementById('show-name').innerHTML = `Your Name is ${event.target.value}`;
+        event.target.value = '';
+    }
+});
