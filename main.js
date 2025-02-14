7de732250dfed6c7d4978254e7777c1daa811117
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 15:40:31 +0600

    falsy value

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-var str = '1000';
-var n = 10;
-console.log(str * n);
-
-console.log(str + n);
-
-
-console.log(Number(str) + n);
-console.log(Number.parseInt(str) + n);
-console.log(n.toString());
-
-console.log(Infinity);
-console.log(String(Infinity));
-console.log(Boolean(-Infinity));
+console.log(Boolean(''));
+console.log(Boolean('fdkfd'));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(54));
+console.log(Boolean(-45));
