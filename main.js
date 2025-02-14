78348dee6bff2cfc516ff47286159d290ee70ea0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 15:44:06 +0600

    type conversion boolean to string . print hex, oct

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ console.log(Boolean(undefined));
 console.log(Boolean(NaN));
 console.log(Boolean(54));
 console.log(Boolean(-45));
+
+console.log(true);
+
+var x = true;
+console.log(x.toString());
+
+var hex = 0xfff;
+var oct = 0o545;
+console.log(hex);
+console.log(oct)
+
+
+
