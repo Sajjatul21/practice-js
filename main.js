1299e69c50e5a7f68603e25172eecff3d5e605d6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 15:34:24 +0600

    type conversion

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-// string lateral
-var str = "string";
-console.log(str);
+var str = '1000';
+var n = 10;
+console.log(str * n);
 
-//string 
+console.log(str + n);
 
-var str1 = String('Shihab');
-var str2 = String(123);
-var str3 = String(3.1234);
-console.log(str1);
-console.log(str2);
-console.log(str3);
 
-var b1 = true;
-var b2 = false;
-console.log("Boolean lateral", b1, b2);
-
-var b3 = Boolean(false);
-var b4 = Boolean(true);
-console.log(b3);
-console.log(b4);
+console.log(Number(str) + n);
+console.log(Number.parseInt(str) + n);
+console.log(n.toString());
