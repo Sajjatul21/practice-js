9b8f9c9243abb4f2e5ae67103d08e2f308678f90
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 15:36:24 +0600

    infinity

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ console.log(str + n);
 console.log(Number(str) + n);
 console.log(Number.parseInt(str) + n);
 console.log(n.toString());
+
+console.log(Infinity);
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
