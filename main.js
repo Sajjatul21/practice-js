0b8cfe38ecfefd99419c1a467198ab0801ff9179
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 10:41:13 +0600

    learn data type

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var name = "Shihab";
-var age = 26;
-console.log("my name is", name);
-console.log("I am " + age + " old");
+// primitive => Primitive type are define by javascript
 
-// var true = 10 // not allow
-var mathNumber = 10;
-var accountNumberDetailsId = 104;
-console.log(mathNumber);
-console.log(accountNumberDetailsId);
+/*
+     1. Number(1,2,3)
+    2. String("This is Text")
+    3. Boolean(true or false)
+    4. undefine
+    5. Null
+ */
+// Object => Object type are user define data type
+
+/*
+    1. Array
+    2. Object
+    3. Function
+*/
