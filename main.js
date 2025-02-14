f9f2a5e52ad428131b0b4eb87a9c0953b2629e83
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 16:17:23 +0600

    date

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-var n = -45.6;
-console.log(Math.abs(n));
+var date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
 
-console.log('floor', Math.floor(n));
-console.log('ceil', Math.ceil(n));
-console.log("round", Math.round(n));
-console.log("Max", Math.max(400, 500, 600));
-console.log("Min", Math.min(400, 500, 600));
-
-console.log(Math.pow(2, 5));
-console.log(Math.sqrt(64));
-
-console.log(Math.random());
-console.log(Math.random() * 10 + 1);
+console.log("full year", date.getFullYear());
+console.log("month", date.getMonth());
+console.log('hour', date.getHours());
+console.log('minute', date.getMinutes());
+console.log("second", date.getSeconds());
