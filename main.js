8fd25168b45810a96fc4614a7c18f1b5d09e0668
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-14 10:29:36 +0600

    again pratctice. variable, print variable. not allow resurved word

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let name = document.getElementById('name');
-// console.log(name);
-name.addEventListener('keypress', function (event) {
-    // console.log(event);
-    if (event.key == "Enter") {
-        // console.log(event.target.value);
-        document.getElementById('show-name').innerHTML = `Your Name is ${event.target.value}`;
-        event.target.value = '';
-    }
-});
+var name = "Shihab";
+var age = 26;
+console.log("my name is", name);
+console.log("I am " + age + " old");
+
+// var true = 10 // not allow
+var mathNumber = 10;
+var accountNumberDetailsId = 104;
+console.log(mathNumber);
+console.log(accountNumberDetailsId);
