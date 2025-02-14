2616f8fd3d98ae6c1cd7016f8e3fb9426070c137
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-15 02:22:39 +0600

    for loop

--- main.js
+++ main.js
@@ -1,38 +1,16 @@
-var n = 10;
-
-var str = '';
-
-if (n % 2 == 0) {
-    str = "Even";
-}
-else {
-    str = "Odd";
-}
-console.log(str);
-
-var result = n % 2 == 0 ? "even" : "Odd";
-console.log(result);
-
-/* var name = '';
-var fullName = "";
-if (name.length == 0) {
-    fullName = "Sajjatul";
-}
-else {
-    fullName = name;
-}
-console.log(fullName);
-
- */
-
-var name = '';
-var fullName = name || "Shihab";
-console.log(fullName);
-
-var isOk = true
-
-if (isOk){
-    console.log("EveryThing ok")
+for (var i = 0; i < 5; i++) {
+    console.log((i + 1), "Sajjatul Islam");
 }
 
-isOk && console.log("Everything is Ok")
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+var sum = 0;
+for (var i = 0; i <= 10; i++) {
+    console.log(sum + " + " + i + " = " + (sum + i));
+    sum += i;
+}
+console.log("Total sum", sum);
