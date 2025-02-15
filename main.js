62e66f166742a4bc2c8b50db43443a7f1a62485e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-15 20:34:48 +0600

    convert number to string many way

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ console.log(str);
 // string constructor
 var str2 = String("Sajjatul Islam");
 console.log(str2);
+
+var n = 10;
+
+var str3 = n + ''
+var str4 = n.toString()
+var str5 = String(n)
+console.log(str3)
+console.log(str4)
+console.log(str5)
