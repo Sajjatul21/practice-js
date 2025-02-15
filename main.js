bc3fd1e1db9fc77c3e783eefec919ea14922c007
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-15 20:24:18 +0600

    break

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-var n = 5;
-for (var i = 1; i <= n; i++) {
-    var result = '';
-    for (var j = 1; j <= i; j++) {
-        // console.log(j);
-        result = result + j + " ";
+var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("Winner winner Chicken Dinner");
+        isRunning = false;
     }
-    console.log(result);
+    else {
+        console.log("You have got", rand);
+    }
 }
 
-for (var i = 1; i <= n; i++) {
-    var result = '';
-    for (var j = 1; j <= n; j++) {
-        result += "* ";
+while (true) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("Winner Winner Chicken Dinner");
+        break;
     }
-    console.log(result);
+    else {
+        console.log("You have got", rand);
+    }
+}
+
+for (var i = 1; i <= 10; i++) {
+    if (i % 5 == 0) {
+        break;
+    }
+    else {
+        console.log(i);
+    }
 }
