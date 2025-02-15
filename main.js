f8e054368a8f7fba8eca71391201a9b2b03db4b3
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-15 20:25:48 +0600

    continue

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-var isRunning = true;
-while (isRunning) {
-    var rand = Math.floor(Math.random() * 10 + 1);
-    if (rand == 9) {
-        console.log("Winner winner Chicken Dinner");
-        isRunning = false;
-    }
-    else {
-        console.log("You have got", rand);
-    }
-}
 
-while (true) {
-    var rand = Math.floor(Math.random() * 10 + 1);
-    if (rand == 9) {
-        console.log("Winner Winner Chicken Dinner");
-        break;
-    }
-    else {
-        console.log("You have got", rand);
-    }
-}
 
 for (var i = 1; i <= 10; i++) {
     if (i % 5 == 0) {
-        break;
+        continue;
     }
     else {
         console.log(i);
