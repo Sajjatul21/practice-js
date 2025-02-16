46c29922f03ea6a144cbe3052610c3b229c4667d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 11:11:53 +0600

    array method

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // var arr = [1, 2, 52, 70, 2, 74, 83, 26];
 var arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-for (var i = 0; i < (arr.length / 2); i++) {
-    var temp = arr[i];
+console.log(arr.join(' '));
+console.log(arr.join(', '));
+console.log(arr.join('| '));
 
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
-
-}
+arr.fill(0);
+console.log(arr);
+arr.fill(true);
 console.log(arr);
 
-console.log(arr.reverse());
+var arr1 = [3, 4, 5, 6];
+var arr2 = [7, 8, 9, 10];
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+console.log(Array.isArray(arr3));
+
+var a = [1, 3];
+/* var b = a
+b[0] = 4 */
+var b = Array.from(a);
+b[0]= 5;
+console.log(a);
+console.log(b)
+
+console.log(Array.from("123"))
