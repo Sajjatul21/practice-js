92c8592421713c3643ca1e997239c61719817698
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 20:10:56 +0600

    first class function

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-var a = 'abc';
-
-if (true) {
-    if (true) {
-        var b = 'I am B';
-        function x() {
-            // var a = 20;
-            function y() {
-                // var a = 10;
-                console.log(a);
-            }
-           
-            y();
-        }
-    }
+function add(a, b) {
+    return a + b;
 }
-x();
+// a function can be stored ina a variable
+var sum = add;
+console.log(sum(3, 4));
+// a function can be stored in an array
+var arr = [];
+arr.push(add);
+console.log(arr);
+console.log(arr[0](2, 2));
+// a function ca be stored in an object
+var obj = {
+    sum: add
+};
+console.log(obj);
+console.log(obj["sum"](3, 3));
+
+// we can create a function as need
+setTimeout(function () {
+    console.log("I have created");
+}, 1000);
