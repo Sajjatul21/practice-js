01266c51e91f2beefcb5c046c16712e3d8b3c685
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 20:18:57 +0600

    higher order function

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 function add(a, b) {
     return a + b;
 }
-// a function can be stored ina a variable
-var sum = add;
-console.log(sum(3, 4));
-// a function can be stored in an array
-var arr = [];
-arr.push(add);
-console.log(arr);
-console.log(arr[0](2, 2));
-// a function ca be stored in an object
-var obj = {
-    sum: add
-};
-console.log(obj);
-console.log(obj["sum"](3, 3));
 
-// we can create a function as need
-setTimeout(function () {
-    console.log("I have created");
-}, 1000);
+
+function manipulated(a, b, func) {
+    var c = a + b;
+    var d = a - b;
+
+    /* function multiply() {
+        var m = func(a, b);
+        return c * d * m;
+    }
+    return multiply; */
+    return function () {
+        var m = func(a, b);
+        return c * d * m;
+    };
+}
+
+var multiply = manipulated(4, 3, add);
+console.log(typeof multiplyj);
+console.log(multiply());
