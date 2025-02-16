c26727dd06d3eb0397b26f3010905005ae562fad
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 10:56:16 +0600

    reverse an array

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 // var arr = [1, 2, 52, 70, 2, 74, 83, 26];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-var arr = [
-    [87, 56, 79, 100],
-    [58, 78, 98, 90],
-    [96, 79, 45, 99]
-];
+for (var i = 0; i < (arr.length / 2); i++) {
+    var temp = arr[i];
 
-/* console.log(arr[0])
-console.log(arr[1])
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
 
-console.log(arr[0][0]) */
+}
+console.log(arr);
 
-
-
-for (var i = 1; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        console.log("Element " + i + ": " + arr[i][j]);
-    }
-} 
+console.log(arr.reverse());
