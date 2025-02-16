777722ddeec720f26b373612b55105d642d46820
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 12:35:13 +0600

    for in operator

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-var obj1 = {
-    a: 10,
-    b: 20
+var obj = {
+    x: 10,
+    y: 20,
+    z: 30
 };
-var obj2 = {
-    a: 10,
-    b: 20
-};
+console.log("x" in obj);
+console.log("p" in obj);
 
-console.log(obj1 == obj2);
-
-if (obj1.a == obj2.a && obj1.b == obj2.b) {
-    console.log(true);
+for (var i in obj) {
+    // console.log(i);
+    // console.log(obj[i]);
+    console.log(i + ": " + obj[i]);
 }
-else {
-    console.log(false);
-}
-
-console.log(obj1);
-console.log(JSON.stringify(obj1));
-
-console.log(JSON.stringify(obj1) == JSON.stringify(obj2));
