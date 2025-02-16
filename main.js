5d989c0328b8c0110d16a34505f9774a68abf20a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 12:17:43 +0600

    access property

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-// var arr = [1, 2, 52, 70, 2, 74, 83, 26];
-var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+var obj = {
+    x: 10,
+    y: 20
+};
+// dot notation
 
-console.log(arr.join(' '));
-console.log(arr.join(', '));
-console.log(arr.join('| '));
+console.log(obj.x);
+// array notation
+console.log(obj["y"]);
 
-arr.fill(0);
-console.log(arr);
-arr.fill(true);
-console.log(arr);
 
-var arr1 = [3, 4, 5, 6];
-var arr2 = [7, 8, 9, 10];
-var arr3 = arr1.concat(arr2);
-console.log(arr3);
-console.log(Array.isArray(arr3));
+var person = {
+    name: "Johan",
+    age: 32
+};
+// console.log(person["name"])
+var props = "name";
+console.log(person[props]);
 
-var a = [1, 3];
-/* var b = a
-b[0] = 4 */
-var b = Array.from(a);
-b[0]= 5;
-console.log(a);
-console.log(b)
-
-console.log(Array.from("123"))
+var person1 = {
+    "first Name": "jack johan",
+    age: 21
+};
+console.log(person1['first Name']);
