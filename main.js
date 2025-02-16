ff03dc61378eb7fa7a41a2887c91c57ae040a02e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 15:57:12 +0600

    sum function

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ function sub(a, b) {
 }
 
 sub(20, 5);
+
+var arr1 = [23, 43, 64];
+var arr2 = [65, 83, 94];
+var arr3 = [5, 85, 90];
+
+var sum = 0;
+for (var i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+}
+console.log(sum);
+
+function sumOfArray(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+sumOfArray(arr1)
+sumOfArray(arr2)
+sumOfArray(arr3)
