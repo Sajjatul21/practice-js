cac1f78ee73eecdb080cca417cf65d8085f99a51
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 12:29:01 +0600

    remove property

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var obj = {
     y: 20
 };
 // set value using dot notation 
-obj.x = 43
-console.log(obj)
+obj.x = 43;
+console.log(obj);
 // console.log(obj.c)
 
-obj['y'] = 100
-var props = 'z'
-obj[props] = 900
-console.log(obj)
+obj['y'] = 100;
+var props = 'z';
+obj[props] = 900;
+console.log(obj);
+delete obj.x;
+console.log(obj);
