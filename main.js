3661e7ab6945627b6ce1486b72fce575c343c896
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 21:06:38 +0600

    map implementation

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 var arr = [1, 2, 3, 4, 5];
-var sum = 0;
-arr.forEach(function (value, index, arr) {
-    // console.log(value, index, arr);
-    sum += value;
+var sqr = arr.map(function (value, index, arr) {
+    // return Math.random ()* 100;
+    return value * value;
 });
-// console.log(sum);
 
-function forEach(arr, callback) {
-    for (var i = 0; i < arr.length; i++) {
-        // console.log(arr[i]);
-        callback(arr[i], i, arr);
-    }
-}
-
-forEach(arr, function (value, index, arr) {
-    console.log(value, index, arr);
-    sum += value;
-});
-console.log(sum);
-
-forEach(arr, function (value, index, arr) {
-    arr[index] = value + 5;
-});
 console.log(arr);
+console.log(sqr);
+
+function map(arr, callback) {
+    var newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        // var temp = arr[i] * arr[i];
+        var temp = callback(arr[i], i, arr);
+        newArray.push(temp);
+    }
+    return newArray;
+}
+var myMap = map(arr, function (value, index, arr) {
+    return value * value;
+});
+console.log(myMap);
+
+var qubArr = map(arr, function (value) {
+    return value * value * value;
+});
+
+console.log(qubArr);
