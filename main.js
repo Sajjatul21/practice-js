f12fbeb26d9042bbd1722e7bdc61a8e0188e2355
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-16 12:27:33 +0600

    set property

--- main.js
+++ main.js
@@ -2,23 +2,12 @@ var obj = {
     x: 10,
     y: 20
 };
-// dot notation
+// set value using dot notation 
+obj.x = 43
+console.log(obj)
+// console.log(obj.c)
 
-console.log(obj.x);
-// array notation
-console.log(obj["y"]);
-
-
-var person = {
-    name: "Johan",
-    age: 32
-};
-// console.log(person["name"])
-var props = "name";
-console.log(person[props]);
-
-var person1 = {
-    "first Name": "jack johan",
-    age: 21
-};
-console.log(person1['first Name']);
+obj['y'] = 100
+var props = 'z'
+obj[props] = 900
+console.log(obj)
