16649560c9b5551eb8b38a2cb0465eaee9e2df89
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-17 08:34:31 +0600

    find

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 var arr = [2, 4, 3, 7, 5, 8, 9, 6];
-var sum = arr.reduce(function (previousValue, currentValue) {
-    return previousValue + currentValue;
+var result = arr.find(function (value) {
+    return value == 9;
 });
-// console.log(sum);
 
-var max = arr.reduce(function (previousValue, currentValue) {
-    return Math.max(previousValue, currentValue);
-}, 0);
-// console.log(max);
+console.log(result);
 
-function reduce(arr, callback, acc) {
+function find(arr, callback) {
     for (var i = 0; i < arr.length; i++) {
-        acc = callback(acc, arr[i]);
+        if (callback(arr[i])) {
+            return arr[i];
+        }
     }
-    return acc;
 }
 
-var sum1 = reduce(arr, function (previousValue, currentValue) {
-    return previousValue + currentValue;
-}, 0);
+var findValue = find(arr, function (value) {
+    return value == 9;
+});
 
-var max1 = reduce(arr, function (previousValue, currentValue) {
-    return Math.max(previousValue, currentValue);
-}, arr[0]);
-
-console.log(max1);
+console.log(findValue);
