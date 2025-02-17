a260b25f7ab1b082f1ddaf7be8177430bde26bf6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-17 08:39:38 +0600

    findIndex function and implementation

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var arr = [2, 4, 3, 7, 5, 8, 9, 6];
-var result = arr.find(function (value) {
-    return value == 9;
-});
 
+var result = arr.findIndex(function (value) {
+    return value % 2 == 0;
+});
 console.log(result);
 
-function find(arr, callback) {
+function findIndex(arr, callback) {
     for (var i = 0; i < arr.length; i++) {
         if (callback(arr[i])) {
-            return arr[i];
+            return i;
         }
     }
 }
 
-var findValue = find(arr, function (value) {
+var findIndexValue = findIndex(arr, function (value) {
     return value == 9;
 });
 
-console.log(findValue);
+console.log(findIndexValue);
