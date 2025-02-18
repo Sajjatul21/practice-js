e312a08aff9b297081802aa2351837d139c960b2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 17:25:16 +0600

    property descriptor

--- main.js
+++ main.js
@@ -1,42 +1,27 @@
-var Rectangle = function (width, height) {
-    this.width = width;
-    this.height = height;
-    // var name = "Sajjatul Islam";
-    var position = {
-        x: 50,
-        y: 20
-    };
-
-    var printProperties = function () {
-        console.log("my width is " + this.width);
-        console.log('My height is ' + this.height);
-    }.bind(this);
-    /*  this.getPosition = function () {
-         return position;
-     }; */
-
-    Object.defineProperty(this, 'position', {
-        get: function () {
-            return position;
-        },
-        set: function (v) {
-            position = v;
-        }
-    });
-
-    this.draw = function () {
-        console.log("I am rectangle");
-        printProperties();
-        console.log('Position:X = ' + position.x + ", position:Y = " + position.y);
-    };
+var person = {
+    name: "Shihab"
 };
 
-var rect = new Rectangle(4, 5);
-// rect.printProperties()error
-rect.draw();
-rect.position = {
-    x: 500,
-    y: 400,
-    z: 600
-};
-console.log(rect.position);
+console.log(person);
+console.log(person.toString());
+
+for (var i in person) {
+    console.log(i);
+}
+
+console.log(Object.keys(person));
+
+var descriptor = Object.getOwnPropertyDescriptor(person, "name");
+console.log(descriptor);
+
+var baseObj = Object.getPrototypeOf(person);
+// console.log(baseObj)
+
+var stringDescriptor = Object.getOwnPropertyDescriptor(baseObj, "toString");
+console.log(stringDescriptor);
+
+Object.defineProperty(person, 'name', {
+    configurable: false,
+    enumerable: false,
+    writable: false
+});
