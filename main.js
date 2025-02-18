216fbbde7b5e402cfd7d38ae8955dcd54a9f7a2e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 15:50:29 +0600

    pass by value pass by referance

--- main.js
+++ main.js
@@ -1,49 +1,21 @@
-/* function myFunc(c, d) {
-    console.log(this.a + this.b + c + d);
-    console.log(this);
-} */
-// myFunc.call({ a: 10, b: 20 });
-/* 
-function myFunc(c, d) {
-    console.log(this.a + this.b + c + d);
-    console.log(this);
+var n = 10;
+function change(n) {
+    n += 100;
+    console.log(n);
 }
-myFunc.apply({ a: 10, b: 20 }, [40, 50]); */
-function myFunc(c, d) {
-    console.log(this.a + this.b + c + d);
-    console.log(this);
-}/* 
-var result = myFunc.bind({ a: 10, b: 20 }, 10, 20);
-console.log(result()) */;
-var testBind = myFunc.bind({ a: 20, b: 30 });
-testBind(50, 60);
 
-var person = {
-    name: "Sajjatul"
+change(n);
+console.log(n);
+
+var obj = {
+    a: 10, b: 20
 };
 
-function greet() {
-    console.log("Hello",  this.name);
-
-}
-var boundGreet = greet.bind(person);
-boundGreet();
-
-
-var person1 = {
-    name: "Shihab"
-}
-function greeting(greeting){
-    console.log(greeting, ", "+ this.name)
-}
-greet.call(person1)
-
-var person2 = {
-    name: "Sajjatu Islam"
+function changeMe(obj) {
+    obj.a = obj.a + 100;
+    obj.b = obj.b + 200;
+    console.log(obj);
 }
 
-function greet2 (greeting, ending){
-    console.log(greeting, ", "+ this.name + ending)
-}
-
-greet.apply(person2, ["Hello", "!"])
+changeMe(obj);
+console.log(obj);
