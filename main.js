7657d1d00793768c1fb5e77faba3de8eefc4e4fb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 08:34:18 +0600

    function creation and executin phase

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-var a = 10;
 
-var b;
-console.log('a is', a);
-console.log('b is ', b);
+abc()
+function abc (){
+    console.log("I am function")
+}
 
-var b = 20;
-console.log('b is ', b);
-console.log('c is', c);
-
-var c = 30;
-
-console.log('c is', c);
+abc()
