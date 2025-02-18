fb48280bd0ea78e31a93222e02da9b35801a5deb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 15:20:39 +0600

    call apply bind

--- main.js
+++ main.js
@@ -17,3 +17,33 @@ var result = myFunc.bind({ a: 10, b: 20 }, 10, 20);
 console.log(result()) */;
 var testBind = myFunc.bind({ a: 20, b: 30 });
 testBind(50, 60);
+
+var person = {
+    name: "Sajjatul"
+};
+
+function greet() {
+    console.log("Hello",  this.name);
+
+}
+var boundGreet = greet.bind(person);
+boundGreet();
+
+
+var person1 = {
+    name: "Shihab"
+}
+function greeting(greeting){
+    console.log(greeting, ", "+ this.name)
+}
+greet.call(person1)
+
+var person2 = {
+    name: "Sajjatu Islam"
+}
+
+function greet2 (greeting, ending){
+    console.log(greeting, ", "+ this.name + ending)
+}
+
+greet.apply(person2, ["Hello", "!"])
