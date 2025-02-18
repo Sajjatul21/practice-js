46186aab92ca1b7d95c4bfe92dce40789b868f95
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 11:24:57 +0600

    tha value of this determined during the execution phase of the execution cntext

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ function myFunc() {
 }
 myFunc();
 new myFunc();
+
+
+var rect = {
+    width: 100,
+    height: 200,
+    draw: function () {
+        console.log("I am a rectangle");
+        this.printProperties();
+        console.log(this);
+    },
+
+    printProperties: function () {
+        console.log("My width is", this.width);
+        console.log("My height is", this.height);
+    }
+};
+
+rect.draw();
+
+var another = {
+    width: 59,
+    height: 63,
+    print: rect.printProperties
+};
+
+another.print();
