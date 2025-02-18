208a7fc279ff21c456cccc2891dcc13450ebd23e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 14:50:42 +0600

    costructor pattern

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-var createReact = function (width, height) {
-    return {
-        width: width,
-        height: height,
+var Rectangle = function (width, height) {
+    this.width = width;
+    this.height = height;
 
-        draw: function () {
-            console.log("I am rectangle");
-            this.printProperties();
-            console.log(this);
-        }
-        ,
-        printProperties: function () {
-            console.log("My width is", width);
-            console.log('My height is', height);
-        }
+    this.draw = function () {
+        console.log("I am rectangle");
+        this.printProperties();
+        console.log(this);
+    };
+    this.printProperties = function () {
+        console.log("My width is", this.width);
+        console.log("My height is", this.height);
     };
 };
 
-var react = createReact(10, 5);
-// console.log(react);
-react.draw()
-
-var react2 = createReact(100, 200)
-console.log(react2)
-react2.draw()
+var rect = new Rectangle(2,3);
+rect.draw();
