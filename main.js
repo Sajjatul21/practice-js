636c6031ea457f5f2f183eec5725ee7d75ad7262
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 11:09:13 +0600

    object literal

--- main.js
+++ main.js
@@ -1,49 +1,15 @@
-var width = 10;
-var height = 20;
-
-function calculateArea(width, height) {
-    return width * height;
-}
-
-function calculateRange(width, height) {
-    return 2 * (width + height);
-}
-
-var area = calculateArea(width, height);
-var range = calculateRange(width, height);
-
 var rect = {
-    width: 10,
-    height: 20,
-    calculateArea: function () {
-        return this.width * this.height;
-    },
-    calculateRange: function () {
-        return 2 * (this.width + this.height);
+    width: 100,
+    height: 50,
+    draw: function () {
+        console.log('I am a rectangle');
+        console.log("My width is ", this.width);
+        console.log("My height is ", this.height);
     }
 };
 
-var area1 = rect.calculateArea();
-var range1 = rect.calculateRange();
+rect.draw();
 
-class ClassName {
-    constructor(property1, property2) {
-        this.property1 = property1;
-        this.property2 = property2;
-    }
-    method1() {
-        console.log(`property1: ${this.property1}`);
-    }
-    method2() {
-        console.log(`Property2: ${this.property2}`);
-    }
-
-    static staticMethod() {
-        console.log('This is a static method');
-    }
-}
-
-const obj = new ClassName("Value1", "Value2");
-obj.method1();
-obj.method2();
-ClassName.staticMethod();
+rect.width = 200;
+rect.height = 500;
+rect.draw();
