b8f6d39dd039f0d152e5e51e422807dd7bb0cd8f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-18 11:14:41 +0600

    this

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-var rect = {
-    width: 100,
-    height: 50,
-    draw: function () {
-        console.log('I am a rectangle');
-        console.log("My width is ", this.width);
-        console.log("My height is ", this.height);
-    }
-};
-
-rect.draw();
-
-rect.width = 200;
-rect.height = 500;
-rect.draw();
+function myFunc() {
+    console.log(this);
+}
+myFunc();
+new myFunc();
