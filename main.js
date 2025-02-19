5850a54930a5af12fc72d069c1c407063a4d7ec5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 19:51:26 +0600

    wen resolve a promise in then block it`s show in nex than block

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ fetch(`${BASE_URL}/user/1`)
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 fetch(`${BASE_URL}/users/1`)
-.then(res=>res.json())
-.then(data =>console.log(data))
-.catch(e=>console.log(e))
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        return Promise.resolve("something")
+    })
+    .then(str=>{
+        console.log(str)
+        return Promise.resolve("Another Promise")
+    })
+    .then(another=>console.log(another))
+    .catch(e => console.log(e));
