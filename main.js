3f2069da57c5c64c9adfa09295b3a289b2fa38eb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 16:47:41 +0600

    module system

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-class Shape {
-    constructor(color) {
-        this.color = color;
-    }
-    draw() {
-        console.log("Drawing");
-    }
-}
-
-class Rectangle extends Shape {
-    constructor(color, width, height) {
-        super(color);
-        this.width = width;
-        this.height = height;
-    }
-    draw() {
-        console.log("Drawing a Rectangle....");
-    }
-    calculate() {
-        return this.width * this.height;
-    }
-}
-
+// import Rectangle from "./Rectangle.js";
+/* 
 let r = new Rectangle("Red", 3, 4);
 console.log(r);
 r.draw();
-console.log(r.color);
+console.log(r.color); */
+
+/* import * as func from "./func.js";
+
+console.log(func);
+ */
+
+import { add, div } from './func.js';
+console.log(add(3, 4));
+
+console.log(div(52, 7));
