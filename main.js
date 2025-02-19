e1eaaacc4aeb9d112223aaa657be7f344896901d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 09:13:56 +0600

    rest and spread operator

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-let obj = {
-    start: 1,
-    end: 5,
-    [Symbol.iterator]: function () {
-        let currentValue = this.start;
-        const self = this;
-        return {
-            next() {
-                return {
-                    done: currentValue > self.end,
-                    value: currentValue > self.end ? undefined : currentValue++
-                };
-            }
-        };
+function sum() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
     }
-};
-
-for (let v of obj) {
-    console.log(v);
+    return sum;
 }
+
+console.log(sum(1, 2, 3, 4, 5));
+
+function sum2(a, b, ...rest) {
+    console.log(a, b);
+    return rest.reduce((a, b) => a + b);
+}
+console.log(sum2(1, 2, 3, 4, 5));
+
+
+var arr = [1, 2, 3, 4, 5];
+console.log(...arr);
+
+var obj = {
+    a: 10,
+    b: 20,
+    c: 30,
+    d: 40
+};
+/* var obj2 = Object.assign({}, obj);
+obj2.e = 50;
+console.log(obj);
+console.log(obj2); */
+
+var obj2 = {
+    ...obj
+};
+obj2.e = 50;
+console.log(obj);
+console.log(obj2);
