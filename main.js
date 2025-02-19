a13c4ddb612d65a7f43ab3ca83e3a4952dfeb401
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 10:44:35 +0600

    destructuring

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-let a = 10, b = 20;
-
-var obj = {
-    a,
-    b,
-    print() {
-        console.log(this);
+let person = {
+    name: "Shihab",
+    email: "shihab@example.com",
+    address: {
+        city: "Chandpur",
+        country: "Bangladesh"
     }
 };
+/* 
+let name = person.name;
+let email = person.email;
+console.log(name);
+console.log(email); */
 
-console.log(obj);
-obj.print();
+let { name, email, address, address: { city, country } } = person;
+console.log(name);
+console.log(email);
+console.log(address);
+console.log(city);
+console.log(country);
