11c685d7e854699a4b712376a18c7c529026efd8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 11:38:29 +0600

    for of is and iterator

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 var arr = [1, 2, 3, 4];
 
-console.log(arr[Symbol.iterator]());
 
-var str = "TEXT";
-console.log(str[Symbol.iterator]());
+for (let v of arr) {
+    console.log(v);
+}
 
-var iterate = arr[Symbol.iterator]();
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-
-let iterateText  = str[Symbol.iterator]()
-console.log(iterateText.next())
-console.log(iterateText.next())
-console.log(iterateText.next())
-console.log(iterateText.next())
-console.log(iterateText.next())
+for (let v of 'Shihab') {
+    console.log(v);
+}
