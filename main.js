509c6be5b5028a71b49a9d19a73336b7beecc705
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 17:47:47 +0600

    asyncronous task should complet in under the asynchronour

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-//synchronous
-console.log("step 1: start");
-console.log("step 2: processing...");
-console.log("step 3: end");
-
-// asynchronous
-
-console.log("Step 1: Start");
-setTimeout(() => {
-    console.log('Step 2: Processing');
-}, 1000);
-console.log("Step 3: End");
-
-console.log("I am line one");
-setTimeout(() => {
-    console.log("i am line tow");
-}, 3000);
-setTimeout(() => {
-    console.log("i am line three");
-}, 0);
-
-console.log("I am line four");
-
-setTimeout(() => {
-    console.log("i am line fivse");
-}, 1000);
+function sayName(name) {
+    // let result;
+    setTimeout(() => {
+        // result = name;
+        console.log("i have done...");
+        console.log(name)
+    }, 3000);
+    // return result;
+}
+/* let output = sayName("Sajjatul Islam");
+console.log(output); */
+sayName("Sajjatul Islam")
