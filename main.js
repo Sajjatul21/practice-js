4e1472ff398ccccdc1c87b987aeaf6241c3a89c7
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 19:21:54 +0600

    xml request with function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const xhr = new XMLHttpRequest();
+/* const xhr = new XMLHttpRequest();
 xhr.open("get", 'https://jsonplaceholder.typicode.com/users');
 
 xhr.onreadystatechange = function (e) {
@@ -13,4 +13,30 @@ xhr.onreadystatechange = function (e) {
     }
 };
 
-xhr.send();
+xhr.send(); */
+
+function getRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                let response = JSON.parse(xhr.response);
+                callback(null, response);
+            }
+            else {
+                callback(xhr.status, null);
+            }
+        }
+    };
+    xhr.send();
+}
+
+getRequest('https://jsonplaceholder.typicode.com/posts', function (err, res) {
+    if (err) {
+        console.loge(err);
+    }
+    else {
+        console.log(res);
+    }
+});
