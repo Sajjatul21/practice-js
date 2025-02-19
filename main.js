b2f8a3c0f1578b4310488a09c8507eb138067b79
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 08:38:23 +0600

    undefine undefine give us nan

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-function testMe() {
-    console.log(this);
+function sqr(n) {
+    return n * n;
 }
-
-testMe();
-
-let obj = {
-    name: "Shihab",
-    print: function () {
-        let self = this;
-        setTimeout(() => {
-            console.log(`Hello ${this.name}`);
-        }, 1000);
-    }
-};
-
-obj.print();
+console.log(sqr());
