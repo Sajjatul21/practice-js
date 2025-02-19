6533ebdc91461b411ade7277c0ba3cfc461253b9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 16:19:07 +0600

    inheritance

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
-const _radius = new WeakMap();
-const _name = new WeakMap();
-const _resize = new WeakMap();
-
-class Circle {
-    constructor(radius, name) {
-        this.size = 100;
-        _radius.set(this, radius);
-        _name.set(this, name);
-        _resize.set(this, () => {
-            console.log(this.size);
-        });
-    }
-    /* getRadius() {
-        return _radius.get(this);
-    } */
-    get radius() {
-        return _radius.get(this);
-    }
-    set radius(v) {
-        _radius.set(this, v);
+class Shape {
+    constructor(color) {
+        this.color = color;
     }
     draw() {
-        console.log("Drawing....");
-        console.log(_radius.get(this), _name.get(this));
-        console.log(this.size);
+        console.log("Drawing");
     }
 }
 
-let c1 = new Circle(2, "Red");
-console.log(c1);
-c1.draw();
-// console.log(c1.getRadius());
-c1.radius = 100;
-console.log(c1.radius);
+class Rectangle extends Shape {
+    constructor(color, width, height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    calculate() {
+        return this.width * this.height;
+    }
+}
+
+let r = new Rectangle("Red", 3, 4);
+console.log(r);
+r.draw();
+console.log(r.color);
