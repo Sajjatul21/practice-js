05e0594573edeba9ba51a5a3593543023535f9ab
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 12:44:26 +0600

    class property

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ class Rectangle {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.another = function(){
+            
+        }
     }
+    name = "Shihab";
+    test = function () {
+
+    };
     draw() {
         console.log("Drawing");
     }
@@ -11,4 +18,4 @@ let rect1 = new Rectangle(45, 65);
 console.log(rect1);
 console.log(rect1.draw());
 console.log(typeof Rectangle);
-console.log(typeof rect1);
+console.log(typeof rect1.name);
