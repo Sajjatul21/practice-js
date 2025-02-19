14fee34878a06f09725cf23a6e5a65c9d244a763
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 17:29:01 +0600

    when try have error its show in catch block

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-function makeWords(text) {
-    try {
-        let str = text.trim();
-        console.log(str);
-        let words = str.split(" ");
-        return words;
-    }
-    catch (e) {
-        // console.log(e);
-        // console.dir(e);
-        // return (e.message);
-        return "please provide a text"
-    }
+try{
+    console.log("I am line 1")
+    throw new Error("I am error")
+    console.log("I am line 2")
+    console.log("I am line 3")
 }
-// let words = makeWords("            kkfjal lkal ;jfja;  kjjj        ");
-let words = makeWords(54);
-console.log(words);
+catch(e){
+    console.log(e.message)
+}
