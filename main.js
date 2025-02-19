8f0162188dd96ad622acac6aa1ceba1c0e09a762
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 10:39:58 +0600

    property and method shorthand

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
-function sum() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-}
-
-console.log(sum(1, 2, 3, 4, 5));
-
-function sum2(a, b, ...rest) {
-    console.log(a, b);
-    return rest.reduce((a, b) => a + b);
-}
-console.log(sum2(1, 2, 3, 4, 5));
-
-
-var arr = [1, 2, 3, 4, 5];
-console.log(...arr);
+let a = 10, b = 20;
 
 var obj = {
-    a: 10,
-    b: 20,
-    c: 30,
-    d: 40
+    a,
+    b,
+    print() {
+        console.log(this);
+    }
 };
-/* var obj2 = Object.assign({}, obj);
-obj2.e = 50;
-console.log(obj);
-console.log(obj2); */
 
-var obj2 = {
-    ...obj
-};
-obj2.e = 50;
 console.log(obj);
-console.log(obj2);
+obj.print();
