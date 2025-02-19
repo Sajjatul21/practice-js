4ae085eaf61c7ccc71597154303af29166113fa1
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 12:00:46 +0600

    set method

--- main.js
+++ main.js
@@ -1,46 +1,32 @@
-function* generator() {
-    yield 1;
-    yield 2;
-    yield 3;
-}
+let set = new Set([1, 2, 3]);
+set.add(4);
+set.add(5);
+set.add(6);
+set.add(1);
 
-let iterate = generator();
-console.log(iterate);
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
+// set.clear()
+// set.delete(5)
+// console.log(set.has(4));
 
+console.log(set.keys());
+console.log(set.values());
 
-var arr = [1, 2, 3];
-function* generator1(collection) {
-    for (let i = 0; i < collection.length; i++) {
-        yield collection[i];
-    }
-}
-let iterate1 = generator1(arr);
-console.log(iterate1.next());
-console.log(iterate1.next());
-console.log(iterate1.next());
-console.log(iterate1.next());
-console.log(iterate1.next());
+let keyIterate = set.keys();
+console.log(keyIterate.next());
+console.log(keyIterate.next());
+console.log(keyIterate.next());
+console.log(keyIterate.next());
+console.log(keyIterate.next());
+console.log(keyIterate.next());
 
 
-let obj = {
-    start: 1,
-    end: 5,
-    [Symbol.iterator]: function* () {
-        let currentValue = this.start;
-        while (currentValue <= this.end) {
-            yield currentValue++;
-        }
-    }
-};
-
-let iterate3 = obj[Symbol.iterator]();
-console.log(iterate3.next());
-console.log(iterate3.next());
-console.log(iterate3.next());
-console.log(iterate3.next());
-console.log(iterate3.next());
-console.log(iterate3.next());
+let valueIterate = set.values();
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
+console.log(valueIterate.next());
