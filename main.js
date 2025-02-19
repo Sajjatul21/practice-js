6c662176fdf0852862df5cb41d16995601c422eb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 10:46:00 +0600

    array destructuring

--- main.js
+++ main.js
@@ -18,3 +18,10 @@ console.log(email);
 console.log(address);
 console.log(city);
 console.log(country);
+
+
+let arr = [1, 2, 3, 4, 5];
+let [first, second, , , last] = arr;
+console.log(first);
+console.log(second);
+console.log(last);
