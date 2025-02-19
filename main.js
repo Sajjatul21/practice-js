3a7fba9a7d46112e09ff488eaee69bdb7b98fa0d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 17:41:07 +0600

    synchronous and asynchronous

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-class CustomError extends Error {
-    constructor(msg) {
-        super(msg);
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, CustomError);
-        }
-    }
-}
+//synchronous
+console.log("step 1: start")
+console.log("step 2: processing...")
+console.log("step 3: end")
 
-try {
-    console.log("I am line 1");
-    throw new CustomError("I am error");
-    console.log("I am line 2");
-    console.log("I am line 3");
-}
-catch (e) {
-    console.log(e.message);
-    // console.dir(e);
-}
-finally {
-    console.log("I am finally block");
-}
+// asynchronous
+
+console.log("Step 1: Start")
+setTimeout(()=>{
+    console.log('Step 2: Processing')
+})
+console.log("Step 3: End")
