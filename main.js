8c08cc0ae3c650e24b9c89813bf26baa9f1a1e6a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 14:56:12 +0600

    static method

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.another = function(){
-            
-        }
+class MathUtility {
+    // 
+    static square(x) {
+        return x * x;
     }
-    name = "Shihab";
-    test = function () {
-
-    };
-    draw() {
-        console.log("Drawing");
+    cube(x) {
+        return x * x * x;
     }
 }
-let rect1 = new Rectangle(45, 65);
-console.log(rect1);
-console.log(rect1.draw());
-console.log(typeof Rectangle);
-console.log(typeof rect1.name);
+
+console.log(MathUtility.square(2));
+
+const obj = new MathUtility();
+console.log(obj.cube(3));
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    static CreateAdmin() {
+        return new User("Sajjatul", 26);
+    }
+}
+
+/* const user = new User("Sajjatul", 26);
+console.log(user); */
+
+const admin = User.CreateAdmin();
+console.log(admin);
+
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    print() {
+        console.log(this.name, this.email);
+    }
+    static create() {
+        let person = JSON.parse(str);
+        return new Person(person.name, person.email);
+    }
+}
+
+let str = '{ "name": "Shhab", "email": "shihab@emial.com" }';
+let p1 = Person.create(str);
+console.log(p1);
+
+console.log(p1 instanceof Person);
+p1.print();
