780a4d3d8441b52ddcd5e37ea3cb22e9a8dc9477
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 08:43:48 +0600

    default parameter has a problem

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ function sqr(n = 1) {
     return n * n;
 }
 console.log(sqr());
+
+function greet(name = "Sajjatul", msg) {
+    console.log(name.length);
+    console.log(`${msg}! ${name}`);
+}
+greet(null, "Hello");
