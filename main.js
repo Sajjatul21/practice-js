e039d78c05080cceaab91fd1263c5b22bb846821
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 09:00:53 +0600

    create custorm iterator

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-function sqr(n = 1) {
-    /* if (!n) {
-        return 1;
-    } */
-    /* n = n || 1; */
-    return n * n;
-}
-console.log(sqr());
+let obj = {
+    start: 1,
+    end: 5,
+    [Symbol.iterator]: function () {
+        let currentValue = this.start;
+        const self = this;
+        return {
+            next() {
+                return {
+                    done: currentValue > self.end,
+                    value: currentValue > self.end ? undefined : currentValue++
+                };
+            }
+        };
+    }
+};
 
-function greet(name = "Sajjatul", msg) {
-    console.log(name.length);
-    console.log(`${msg}! ${name}`);
+for (let v of obj) {
+    console.log(v);
 }
-greet(null, "Hello");
