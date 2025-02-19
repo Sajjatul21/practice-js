6d42bfb7038b944270d36414c5ab8bf822dd0fba
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 16:12:59 +0600

    getter and setter

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ class Circle {
             console.log(this.size);
         });
     }
+    /* getRadius() {
+        return _radius.get(this);
+    } */
+    get radius() {
+        return _radius.get(this);
+    }
+    set radius(v) {
+        _radius.set(this, v);
+    }
     draw() {
         console.log("Drawing....");
         console.log(_radius.get(this), _name.get(this));
@@ -20,4 +29,7 @@ class Circle {
 
 let c1 = new Circle(2, "Red");
 console.log(c1);
-c1.draw()
+c1.draw();
+// console.log(c1.getRadius());
+c1.radius = 100;
+console.log(c1.radius);
