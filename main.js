7f80a8f06cd911f1883ace33a84621ae3dd6807b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-19 10:48:35 +0600

    entries and from entries

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-let person = {
-    name: "Shihab",
-    email: "shihab@example.com",
-    address: {
-        city: "Chandpur",
-        country: "Bangladesh"
-    }
+var obj = {
+    a: 10,
+    b: 20
 };
-/* 
-let name = person.name;
-let email = person.email;
-console.log(name);
-console.log(email); */
 
-let { name, email, address, address: { city, country } } = person;
-console.log(name);
-console.log(email);
-console.log(address);
-console.log(city);
-console.log(country);
+console.log(Object.entries(obj));
 
+var objArr = [
+    ["a", 10],
+    ["b", 20]
+]
 
-let arr = [1, 2, 3, 4, 5];
-let [first, second, , , last] = arr;
-console.log(first);
-console.log(second);
-console.log(last);
+console.log(Object.fromEntries(objArr))
+
