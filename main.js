2ba28cf582de1f6e70d2e4f1e6a088f68094f03b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-20 23:04:24 +0600

    create element

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-let listItem = document.getElementsByTagName('li')
-// console.log(listItem)
-// let listItems = Array.from(listItem)
-// console.log(listItems)
-let listItems =[...listItem]
-listItems.forEach((i, index)=>{
-    console.log(i,index)
-    let text= i.innerHTML
-    i.innerHTML =   `${index+ 1} ${text}`
-})
+let li = document.createElement("li");
+console.dir(li);
+li.className = 'list-group-item';
+li.innerHTML = 'Four';
+let list = document.getElementById('list');
+list.appendChild(li);
