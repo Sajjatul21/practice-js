7a38c291751bf07c96c114de2b7d6d4180bba0c2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:38:52 +0600

    pow, sqrt, random

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ console.log(Math.round(n))
 console.log(Math.max(4, 6,5))
 console.log(Math.min(8,4,6))
 
+console.log(Math.pow(3,4))
+console.log(Math.pow(3,7))
+
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(64))
+
+console.log(Math.random())
+
+console.log(Math.round(Math.random()*10 + 1))
