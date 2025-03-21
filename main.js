6e83512082f629df122cddd6f492d738e35b6643
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:23:20 +0600

    string

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ var str2 = 'string2';
 var str3 = 'string3';
 console.log(str, str2, str3);
 
+var str4 = String("fafd");
+var str5 = String(124);
+var str6 = String(3.1416);
+console.log(str4, str5, str6);
