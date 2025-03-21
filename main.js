de02f573a7e6048605ca458a12f48ed558f8334c
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:41:16 +0600

    git comment

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-var date = new Date();
-
-console.log(date);
-console.log(date.toDateString());
-console.log(date.toTimeString());
-console.log(date.toLocaleString());
+// single line comment
 
 
-console.log("full year", date.getFullYear());
+// var date = new Date();
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleString());
+
+
+/* multi line comment */
+
+/* console.log("full year", date.getFullYear());
 console.log("month", date.getMonth());
 console.log("date", date.getDate());
 console.log("hour", date.getHours());
 console.log("minute", date.getMinutes());
-console.log("second", date.getSeconds());
+console.log("second", date.getSeconds()); */
