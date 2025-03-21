4fdcfdce31ed7b088189a3600c3744b495f7c3b2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:33:03 +0600

    operator

--- main.js
+++ main.js
@@ -1,5 +1,62 @@
-var hex = 0xff;
-console.log(hex);
+// var a = 11;
+// var b = 3;
+// console.log(a % b);
 
-var oct = 0o564;
-console.log(oct);
+// /* Incremental */
+
+// //  Pre incremental
+// console.log(++a)
+
+// // Post incremental
+// console.log(a++)   //post increment: First the output will show then increase
+// console.log(a)
+
+// // Pre Decrement 
+// console.log(--b)
+
+// // Post Decrement
+// console.log(b--)
+// console.log(b)
+
+
+
+// Assignment Operator
+
+var a = 10;
+var b = 20;
+
+a += b;  // a = a+b
+console.log(a);
+
+a -= b;
+console.log(a);
+
+a *= b;
+console.log(a);
+
+a /= b;
+console.log(a);
+
+a %= b;
+console.log(a);
+
+var num1 = 40;
+var num2 = 30;
+
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+num1 = 30;
+
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+var c = '50';
+var d = 50;
+console.log(c == d);
+console.log(c === d);
+
+// typeof Operator
+console.log(typeof d)
