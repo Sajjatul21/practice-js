5d91ce1b5a77ded5ee240c88daf161d19dd7194d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:41:58 +0600

    four different conition

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-// single line comment
-
-
-// var date = new Date();
-// console.log(date);
-// console.log(date.toDateString());
-// console.log(date.toTimeString());
-// console.log(date.toLocaleString());
-
-
-/* multi line comment */
-
-/* console.log("full year", date.getFullYear());
-console.log("month", date.getMonth());
-console.log("date", date.getDate());
-console.log("hour", date.getHours());
-console.log("minute", date.getMinutes());
-console.log("second", date.getSeconds()); */
+/* 
+    1. if condition
+2. else condition
+3. else if condition
+4. switch statement
+*/
