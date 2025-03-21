0ef465b04a6b0a33be8145674807319416f6e65d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:26:25 +0600

    null and undefine

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-var b1 = true;
-var b2 = false;
-
-console.log("boolean lateral: ", b1, b2)
-var b3 = Boolean(true)
-var b4 = Boolean(false)
-console.log('constructor boolean', b3, b4)
+var abc;
+var xyz = null;
+console.log(abc);
+console.log(xyz);
