e06bb7567475be0e712d1544f58039cfc202f4d9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:15:57 +0600

    data type

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-let li = document.createElement("li");
-console.dir(li);
-li.className = 'list-group-item';
-li.innerHTML = 'Four';
-let list = document.getElementById('list');
-list.appendChild(li);
+/ primitive =>  Primitive type are defined by Javascript;
+
+1. Number(1, 2, 3, 6.7);
+2. String("This is text");
+3. Boolean(True or False);
+4. undefined;
+5. null;
+
+// object => object type are user defined data type 
+
+1. Array;
+2. Object;
+3. Function; 
