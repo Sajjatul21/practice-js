4800818411519718a7087fa779544e329d704fc0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:46:04 +0600

    if condition

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-/* 
-    1. if condition
-2. else condition
-3. else if condition
-4. switch statement
-*/
+var a = 20;
+var b = 10;
+
+if (a > b) {
+    console.log('a is greater then b');
+}
+
+if (a < b) {
+    console.log("b is greater than a");
+}
+
+
+var n = 5;
+if (n % 2 == 0) {
+    console.log(n + " is Even number");
+}
+if (n % 2 == 1) {
+    console.log(n + " is Odd number");
+}
