aaa609dfd5f8c7aae719e7ad3a02604c93119695
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-21 23:46:58 +0600

    else condition

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-var a = 20;
-var b = 10;
+var a = 10;
+var b = 20;
 
 if (a > b) {
-    console.log('a is greater then b');
+    console.log("A is greater than B");
 }
-
-if (a < b) {
-    console.log("b is greater than a");
+else {
+    console.log("B is greater than A");
 }
-
 
 var n = 5;
 if (n % 2 == 0) {
-    console.log(n + " is Even number");
+    console.log(n + " is Even Number");
 }
-if (n % 2 == 1) {
-    console.log(n + " is Odd number");
+else {
+    console.log(n + ' is Odd Number');
 }
