d823e66a29f7b34b4faba9112e33ae5f57ebead4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:10:58 +0600

    nested loop

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var n = 5;
 
-var isRunning = false;
-
-do {
-    console.log("Iam running");
+for (var i = 1; i <= n; i++) {
+    var result = '';
+    for (var j = 1; j <= i; j++) {
+        result = result + j + " ";
+    }
+    console.log(result);
 }
-while(isRunning)
