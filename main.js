c05a5b3df860bdb8988555bcf2f10af70fbf4051
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:30:52 +0600

    convert string

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ console.log(str1);
 
 var str2 = String('sajjatul');
 console.log(str2);
+
+var n = 10;
+
+var str3 = n + '';
+var str4 = n.toString();
+
+var str5 = String(n);
+console.log(str3);
+console.log(str4);
+console.log(str5);
