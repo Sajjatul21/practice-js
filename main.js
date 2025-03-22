363256338e641e4c31ea5e12cbfef862fbf67ce8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:34:45 +0600

    another string method

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ console.log(c);
 
 var d = c.substr(5, 4);
 console.log(d);
+
+console.log(c.charAt(3));
+console.log(c.startsWith("I am"));
+console.log(c.endsWith("sj32"));
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log('           Sajjatul islam       '.trim());
+
+// split () give us array  break the string
+console.log(c.split(' '));
