29bb6b49bf5653dad050e711dbfc153de9a0def7
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:01:40 +0600

    while

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-for (var i = 0; i < 10; i++) {
-    console.log((i + 1) + 'Sajjatul islam');
+var i = 0;
+while (i < 5) {
+    console.log(i, 'shihab');
+    i++;
 }
-
-for (var i = 1; i <= 10; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-}
-
-
-var sum = 0;
-for (var i = 1; i <= 10; i++) {
-
-    console.log(sum + " + " + i + " = " + (sum + i));
-    sum += i; // sum = sum + i;
-}
-console.log("total sum", sum);
