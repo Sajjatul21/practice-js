92caccc6321715e7aa4373963f6a064749d3724b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:33:33 +0600

    sub str

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var str = 'This is a \'String\'';
-console.log(str)
-var str1 = 'This is a \nString';
-console.log(str1)
-var str2 = 'This is a \tString';
-console.log(str2)
-var str3 = 'This is a \\String';
-console.log(str3)
+var a = "I am";
+var b = "Shihab";
+var c = a.concat(" " + b);
+
+console.log(c);
+
+var d = c.substr(5, 4);
+console.log(d);
