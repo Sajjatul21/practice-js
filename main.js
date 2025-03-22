db51a734afac1c6ab5e1c789b1803b7a9d05433a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:23:24 +0600

    break

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-var n = 5;
-
-for (var i = 1; i <= n; i++) {
-    var result = '';
-    for (var j = 1; j <= i; j++) {
-        result = result + j + " ";
+while (true) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("Winner Winner Chicken Dinner");
+        break;
     }
-    console.log(result);
-}
-
-for (var i = 1; i<=n; i++){
-    var result = ''
-    for (var j = 1; j<=n; j++){
-        result +='* '
+    else {
+        console.log('You have got', rand);
     }
-    console.log(result)
 }
