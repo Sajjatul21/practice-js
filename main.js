e186f46dec3641236c5a250766b1a39b68d08ca9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 22:55:16 +0600

    ternary operator

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-/*  Logical Operator
-
-1.&&
-2.||
-3. !
-
-*/
-
-var a = 10;
-var b = 30;
-var c= 20;
-
-if(a > b && a>c){
-    console.log(" a is greater than b and c")
+var n = 10;
+var str = '';
+if (n % 2 == 0) {
+    str = 'even';
 }
-else if(b>a && b> c){
-    console.log("b is greater than a and c")
+else {
+    str = 'odd';
 }
-else{
-    console.log('c i large')
-}
+console.log(str);
 
-
-var a = 50;
-var b = 30;
-var c= 20;
-
-if(a > b || a>c){
-    console.log("a is greater than b and c")
-}
-else if(b>a || b> c){
-    console.log("b is greater than a and c")
-}
-else{
-    console.log('c i large')
-}
-
-var check = !(a>b)
-console.log(check)
+var result = n % 2 == 0 ? 'Even' : "odd";
+console.log(result);
