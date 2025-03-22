a50e0aed6c5037c2fc6cf9da5f39219c86615ff1
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:28:55 +0600

    string

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-for (; ;) {
-    var rand = Math.floor(Math.random() * 10 + 1);
-    if (rand == 9) {
-        console.log('winner winner chicken dinner');
-        break;
-    }
-    else {
-        console.log("you have got", rand);
-    }
+var str1 = 'shihab';
+console.log(str1);
 
-
-}
+var str2 = String('sajjatul');
+console.log(str2);
