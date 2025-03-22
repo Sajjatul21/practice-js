3403b469ff31cfbe869b12551a28b05c9911e7e0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-22 23:27:43 +0600

    infinity for

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-while (true) {
+for (; ;) {
     var rand = Math.floor(Math.random() * 10 + 1);
     if (rand == 9) {
-        console.log("Winner Winner Chicken Dinner");
+        console.log('winner winner chicken dinner');
         break;
     }
     else {
-        console.log('You have got', rand);
+        console.log("you have got", rand);
     }
-}
 
-for (var i = 1; i <= 10; i++) {
-    if (i == 3|| i == 7) {
-        continue;
-    }
-    else {
-        console.log(i);
-    }
+
 }
