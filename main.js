1eb67dc0a9e586baf1d614f89d07da176adad340
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:51:39 +0600

    array fill

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(arr.join(' '));
 console.log(arr.join(', '));
 console.log(arr.join('| '));
+
+arr.fill(0);
+
+console.log(arr);
+
+arr.fill(true);
+console.log(arr);
