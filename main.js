fa4b92c7e3ec1fd26dc59a5d835fac9fd413a019
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:44:24 +0600

    if data is not found the data not found show one time

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var arr = [1, 2, 52, 6, 2, 74, 83, 26];
 
 var find = 10;
-
+var isFound = false;
 for (var i = 0; i < arr.length; i++) {
     if (arr[i] == find) {
         console.log('date find at index ', i);
-        break
-    }
-    else{
-        console.log('date is not found')
+        isFound = true;
+        break;
+
     }
 }
+if (!isFound) {
+    console.log('Date not found');
+}   
