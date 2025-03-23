0a4ce3f67225569040dc22f5148f371db86acf96
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:31:36 +0600

    array teaverse

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-var arr = [1, 2, 3, 4];
-console.log(arr);
-
-console.log(arr[0]);
-console.log(arr[1]);
-arr[2] = 7;
-console.log(arr);
-
-var arr2 = Array();
-console.log(arr);
-
-var arr3 = Array(10, 20, 30);
-console.log(arr3);
+var arr = [1, 4, 3, 9, 8, 5, 7];
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
