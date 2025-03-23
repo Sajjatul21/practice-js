c8d4764b2811b8d16986ab30a04fee2bb92272a9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:20:04 +0600

    array lateral

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-var str = 'some string';
-console.log(str.charAt(0));
+var arr = [1, 2, 3, 4];
+console.log(arr);
 
-var str2 = str.charAt(2);
-console.log(str2);
-
-var str3 = str.charAt(100);
-console.log(str3);
-console.log(typeof str3);
-
-
-str = 'Some String';
-var length = 0;
-
-while (true) {
-    if (str.charAt(length) == '') {
-        break;
-    }
-    else {
-        length++;
-    }
-}
-console.log(length);
-
-//easy way to find out string length
-console.log('Sajjatul Islam'.length);
+console.log(arr[0]);
+console.log(arr[1]);
+arr[2] = 7;
+console.log(arr);
