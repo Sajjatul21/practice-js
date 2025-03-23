c55e6e51fa4ca7105b34f1d8a85f410e33cee9fa
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:48:46 +0600

    array reverse

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-var arr = [
-    [87, 56, 79, 100],
-    [58, 78, 98, 90],
-    [96, 79, 45, 99]
-];
-// console.log(arr);
+for (var i = 0; i < (arr.length / 2); i++) {
+    var temp = arr[i];
 
-/* console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
- */
-
-/* console.log(arr[0][0]);
-console.log(arr[1][2]);
-console.log(arr[2][1]); */
-
-
-for (var i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
-    for (var j = 0; j < arr[i].length; j++) {
-        console.log("Element " + i + ": " + arr[i][j]);
-    }
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
 }
+console.log(arr);
+
+console.log(arr.reverse);
