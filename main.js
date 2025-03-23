b4827b92895b1e42a1e1267126674ff817dac3cf
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-23 23:38:05 +0600

    add element in array

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var arr = [1, 4, 3, 9, 8, 5, 7];
-for (var i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) {
-        console.log(arr[i]);
-    }
-}
+arr[2] = 3;
+console.log(arr);
+
+arr.push(9);
+console.log(arr);
+arr.unshift(20);
+console.log(arr);
+
+/*  array.split(startIndex, delete count, addItem1, addItem2) */
+
+arr.splice(2, 0, 1000);
+console.log(arr);
