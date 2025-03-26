f548fd48493ae44b2c805e457179e78f4cb25e05
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:30:35 +0600

    set value

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var obj = {
 console.log(typeof obj);
 
 // object ltaral
- var point = {
-     x: 10,
-     y: 20
- };
- 
+var point = {
+    x: 10,
+    y: 20
+};
+
+point.x = 5;
+point.y = 39;
+console.log(point);
