8f45d86b987b90f1a6827b62a74f4832455cbe6d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:38:33 +0600

    set value using dot and array notation

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-var obj = {
+var point = {
     x: 10,
     y: 20
 };
 
-console.log(obj.x);
-console.log(obj.y);
-
-var person = {
-    name: "johan",
-    age: 32
-};
-
-var props = 'name';
-console.log(person[props]);
+point.x = 3242;
+point.y = 100;
+console.log(point);
+console.log(point.d);
 
 
-var person1 = {
-    "fast Name": "jack johan ",
-    age: 32
-};
-
-console.log(person1['fast Name']);
+point['y'] = 500;
+var props = 'z';
+point[props] = 900;
+console.log(point);
