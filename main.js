89a8c29cce1592116cb84fcbebe9385f4660a2d2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:44:47 +0600

    in operator

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-var obj1 = {
-    a: 10,
-    b: 20
+var obj = {
+    x: 10,
+    y: 20,
+    z: 30
 };
-var obj2 = {
-    a: 10,
-    b: 20
-};
-
-console.log(obj1 === obj2);
-
-if (obj1.a === obj2 && obj1.b === obj2.b) {
-    console.log(true);
-}
-else {
-    console.log(false);
-}
-
-
-console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+console.log("x" in obj)
+console.log("p" in obj)
