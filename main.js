03e0a4330e45eea9860b6423a609db7560fca2d6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:32:05 +0600

    object constructor

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ var point = {
 point.x = 5;
 point.y = 39;
 console.log(point);
+
+var obj = Object();
+obj.a = 10;
+console.log(obj);
+
+var obj1 = new Object();
+obj1.b = 20;
+console.log * (obj1);
