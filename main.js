e1eee42257038f386d4a28085b34e18da9f13af6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:34:03 +0600

    accessing value dot notation

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 var obj = {
-};
-console.log(typeof obj);
-
-// object ltaral
-var point = {
     x: 10,
     y: 20
 };
 
-point.x = 5;
-point.y = 39;
-console.log(point);
-
-var obj = Object();
-obj.a = 10;
-console.log(obj);
-
-var obj1 = new Object();
-obj1.b = 20;
-console.log * (obj1);
+console.log(obj.x);
+console.log(obj.y);
