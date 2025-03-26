ed2e4e1322cdf6c3008472651ee159223d918c97
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:36:05 +0600

    array notation

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ var obj = {
 
 console.log(obj.x);
 console.log(obj.y);
+
+var person = {
+    name: "johan",
+    age: 32
+};
+
+var props = 'name';
+console.log(person[props]);
+
+
+var person1 = {
+    "fast Name": "jack johan ",
+    age: 32
+};
+
+console.log(person1['fast Name']);
