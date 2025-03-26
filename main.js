e98ff55c826fa07534d65584687622b207ede709
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:48:54 +0600

    memor location problem

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ var obj = {
 console.log(Object.keys(obj));
 console.log(Object.values(obj));
 console.log(Object.entries(obj));
+
+
+var obj1 = {
+    x: 10,
+    y: 20,
+    z: 30
+};
+var obj2 = obj1;
+obj2.x = 100;
+console.log(obj1);
+console.log(obj2);
