d1a9e6cf12b23aee62a03daea74af95a309c9f16
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-26 23:29:39 +0600

    object literal

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(arr.join(' '));
-console.log(arr.join(', '));
-console.log(arr.join('| '));
+var obj = {
+};
+console.log(typeof obj);
 
-arr.fill(0);
-
-console.log(arr);
-
-arr.fill(true);
-console.log(arr);
-
-var arr1 = [3, 4, 5, 6];
-var arr2 = [7, 8, 9, 10];
-var arr3 = arr1.concat(arr2);
-console.log(arr3);
-
-console.log(Array.isArray(arr));
-
-var a = [1, 3];
-var b = Array.from(a);
-b[0] = 2;
-console.log(a);
-console.log(b);
-
+// object ltaral
+ var point = {
+     x: 10,
+     y: 20
+ };
+ 
