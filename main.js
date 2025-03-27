77d6b924c9ea27b79dca565f870a3e3439e1181a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:11:24 +0600

    argument give us array like object. we can traverse argument

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-function add(a, b) {
-    var result = a + b;
-    console.log(result);
+function test(a, b, c) {
+    console.log(arguments);
 }
-add(10, 20);
-
-
-
-var arr = [23, 43, 64];
-
-var sum = 0;
-for (var i = 0; i < arr.length; i++) {
-    sum += arr[i];
-}
-
-console.log(sum);
-
-function sumOfArray(arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    console.log(sum);
-}
-
-var arr2 = [65, 83, 94, 89];
-var arr3 = [5, 85, 90, 100, 200];
-sumOfArray(arr2);
-sumOfArray(arr3);
+test(1, 2, 3);
