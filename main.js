68ebf1820f50439f8eba68a8c8a1595fbe7685c8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:27:45 +0600

    another example

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ function x() {
     y();
 }
 x();
+
+function test(n) {
+    function a() {
+        return n % 3 == 0;
+    }
+
+    function b() {
+        return n % 5 == 0;
+    }
+    if (a() && b()) {
+        console.log(n, 'is divisible by both 3 and 5');
+    }
+    else {
+        console.log('not a valid number');
+    }
+}
+
+test(10);
+test(15);
