72fc1068f196791dd6b91f74a4c2a58b7064d173
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:05:20 +0600

    parameter and argument

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-function functionName() {
-    console.log("I am a function");
+function add(a, b) {
+    var result = a + b;
+    console.log(result);
 }
+add(10, 20);
 
-function add() {
-    var a = 10;
-    var b = 20;
-    console.log(a + b);
-}
-
-function sub() {
-    var a = 10;
-    var b = 20;
-    console.log(a - b);
-}
-
-functionName();
-
-for (var i = 5; i < 5; i++) {
-    functionName();
-}
-
-add();
-sub();
