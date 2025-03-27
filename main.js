301c00c90937d9cebb20d779db7e48dd767418ec
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:30:09 +0600

    pure function

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-//  what is function programming
+//what is Pure function
+// it given the same result if given the same arguments
+// It does not cause any observable side effect
 
-/* 
-    Functional programming is a programming programming paradigm - stye of balding the structure and elements of computer programs -that
-    treat commotion as evaluation of mathematical functional avoids changing0state and mutable data- wikipedia
-*/
-
-/* three main terms 
-    1. pure function
-    2. First Class function
-    3. Higher Order Function
-
-*/
+function sqr(n) {
+    return n * n;
+}
+console.log(sqr(2));
+console.log(sqr(2));
+console.log(sqr(2));
