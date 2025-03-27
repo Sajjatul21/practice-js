52ce1ad823b19f24189b9909661ab653939aa983
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:09:58 +0600

    create a functon to sum all array element

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ for (var i = 0; i < arr.length; i++) {
 }
 
 console.log(sum);
+
+function sumOfArray(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+
+var arr2 = [65, 83, 94, 89];
+var arr3 = [5, 85, 90, 100, 200];
+sumOfArray(arr2);
+sumOfArray(arr3);
