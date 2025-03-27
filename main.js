2c631e3f514ca74d932f33275502822350df5b9f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-27 23:12:55 +0600

    when we use agrument we not need use parameter. but use parameter use best practice

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function test(a, b, c) {
-    console.log(arguments);
+    // console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
 }
 test(1, 2, 3);
